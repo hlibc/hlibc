e02f636a4db44abc174dc4f2e720ae294f2f2b09
cmgraff <cm0graff@gmail.com>
2017-07-02 17:53:58 -0500

    remove complex/ and math/*/*.s

--- src/math/i386/llrintf.s
+++
@@ -1,9 +0,0 @@
-.global llrintf
-.type llrintf,@function
-llrintf:
-	sub $8,%esp
-	flds 12(%esp)
-	fistpq (%esp)
-	pop %eax
-	pop %edx
-	ret
--- src/math/x86_64/llrintl.s
+++
@@ -1,7 +0,0 @@
-.global llrintl
-.type llrintl,@function
-llrintl:
-	fldt 8(%rsp)
-	fistpq 8(%rsp)
-	mov 8(%rsp),%rax
-	ret
