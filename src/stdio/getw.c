9388a0f85db646ace59ba644f7098a586b604361
cmgraff <cm0graff@gmail.com>
2017-07-02 18:54:36 -0500

    remove as much wide char support as possible

--- src/stdio/getw.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int getw(FILE *f)
-{
-	int x;
-	return fread(&x, sizeof x, 1, f) ? x : EOF;
-}
--- src/stdio/putw.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int putw(int x, FILE *f)
-{
-	return fwrite(&x, sizeof x, 1, f) ? x : EOF;
-}
