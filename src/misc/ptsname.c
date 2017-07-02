7bb7ec1f8d1c14b5827b15040d072d50e177f2cd
cmgraff <cm0graff@gmail.com>
2017-07-02 17:02:13 -0500

    just keep a tiny bit of misc

--- src/misc/ptsname.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-int __ptsname_r(int, char *, size_t);
-
-char *ptsname(int fd)
-{
-	static char buf[9 + sizeof(int)*3 + 1];
-	return __ptsname_r(fd, buf, sizeof buf) < 0 ? 0 : buf;
-}
