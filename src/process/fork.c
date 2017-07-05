2ad04d7267112e694151dc17965d46615d48f4ef
cmgraff <cm0graff@gmail.com>
2017-07-05 08:40:01 -0500

    remove thread foo bar from fork

--- src/process/fork.c
+++ src/process/fork.c
@@ -1,26 +1,10 @@
 #include <unistd.h>
 #include "syscall.h"
 #include "libc.h"
-#include "pthread_impl.h"
-
-static void dummy(int x)
-{
-}
-
-weak_alias(dummy, __fork_handler);
 
 pid_t fork(void)
 {
 	pid_t ret;
-	__fork_handler(-1);
 	ret = syscall(SYS_fork);
-	/* removed thrd stuff here CM Graff */
-
-
-
-
-
-
-	__fork_handler(!ret);
 	return ret;
 }
