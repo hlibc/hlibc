54103f581a44c32de7534a15258c1b876bc5b85e
cmgraff <cm0graff@gmail.com>
2017-07-05 07:43:47 -0500

    don't use _cp

--- src/network/sendto.c
+++ src/network/sendto.c
@@ -4,5 +4,5 @@
 
 ssize_t sendto(int fd, const void *buf, size_t len, int flags, const struct sockaddr *addr, socklen_t alen)
 {
-	return socketcall_cp(sendto, fd, buf, len, flags, addr, alen);
+	return socketcall(sendto, fd, buf, len, flags, addr, alen);
 }
