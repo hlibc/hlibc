0a35f6634d892d9c6a2701fbf8b32fb226d62f57
cmgraff <cm0graff@gmail.com>
2017-07-05 22:02:07 -0500

    improve tests

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ printf "\ngrafmusl is logging to buildlog\n\n"
 make -j4 > buildlog 2>&1
 
 make install
-
+echo "==========COMPILING TEST SUITE=================================="
 ${TOOLING}/bin/musl-gcc tests/cat.c -o cat_dynamic
 
 ${TOOLING}/bin/musl-gcc tests/cat.c -o cat_static -static
@@ -23,6 +23,7 @@ ${TOOLING}/bin/musl-gcc tests/malloc-driver.c -o malloc_driver -static -lm
 ${TOOLING}/bin/musl-gcc tests/popen-driver.c -o popen_driver -static -lm 
 
 gcc tests/malloc-driver.c -o control_malloc_driver -static -lm
+echo "============================================================="
 
 echo "==========TEST SUITE START=================================="
 
@@ -38,4 +39,9 @@ diff Makefile diff1 2>&1 > testerr && echo "The \`cat' utility successfully copi
 
 diff diff2 diff3 2>&1 > testerr && echo "The \`malloc_driver' test utility successfully iterated through a dir and compared equal to its control method" || echo "malloc driver failed"
 
+echo "============================================================"
+
+echo "============================================================"
+echo "   If all was successful your compiler will be located at:"
+echo "              ${TOOLING}/bin/musl-gcc"
 echo "============================================================"
--- tests/pow_test.c
+++ tests/pow_test.c
@@ -5,5 +5,6 @@
 int main(void)
 {
 	double n = 4;
-	printf("This number should be (16): %lf\n", pow(n, 2));
+	printf("This number should be (16.000000): %lf    (4^3)\n", pow(n, 2));
+	printf("This number should be (15.580524): %lf    (3.3^2.3)\n", pow(3.3, 2.3));
 }
