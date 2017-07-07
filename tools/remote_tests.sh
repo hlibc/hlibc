d3bbb4a161913f3b0da726e45ad442d52602214b
cmgraff <cm0graff@gmail.com>
2017-07-07 08:50:49 -0500

    add automatic remote tests

---
+++ tools/remote_tests.sh
@@ -0,0 +1,16 @@
+COMMAND='rm -rf grafmusl ; git clone https://www.github.com/cmgraff/grafmusl; cd grafmusl ; ./build.sh'
+
+test_i386()
+{
+	ssh cgraff1@gcc45.fsffrance.org -f "$COMMAND"
+}
+test_i386 > test_i386_log 2>&1
+
+
+test_x86_64()
+{
+	ssh cgraff1@gcc75.fsffrance.org -f "$COMMAND"
+}
+test_x86_64 > test_x86_64_log 2>&1
+
+
