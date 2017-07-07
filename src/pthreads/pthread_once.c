f58891735b1098f9e6ae6c48cd073cb396ed489f
cmgraff <cm0graff@gmail.com>
2017-07-07 17:33:06 -0500

    comment out threads

--- src/pthreads/pthread_once.c
+++ src/pthreads/pthread_once.c
@@ -2,22 +2,22 @@
 
 static void undo(void *control)
 {
-	a_store(control, 0);
-	__wake(control, 1, 0);
+	//a_store(control, 0);
+	//__wake(control, 1, 0);
 }
 
 int pthread_once(pthread_once_t *control, void (*init)(void))
 {
-	static int waiters;
+	//static int waiters;
 
 	/* Return immediately if init finished before */
-	if (*control == 2) return 0;
+	//if (*control == 2) return 0;
 
 	/* Try to enter initializing state. Three possibilities:
 	 *  0 - we're the first or the other cancelled; run init
 	 *  1 - another thread is running init; wait
 	 *  2 - another thread finished running init; just return */
-
+	/*
 	for (;;) switch (a_swap(control, 1)) {
 	case 0:
 		pthread_cleanup_push(undo, control);
@@ -34,4 +34,6 @@ int pthread_once(pthread_once_t *control, void (*init)(void))
 		a_store(control, 2);
 		return 0;
 	}
+	*/
+	return 0;
 }
