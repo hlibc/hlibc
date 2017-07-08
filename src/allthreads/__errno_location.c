dc8aaabfeef84a07d583c2a0523d41c1f1d3d8de
cmgraff <cm0graff@gmail.com>
2017-07-07 19:46:05 -0500

    hollow more threads out

--- src/allthreads/__errno_location.c
+++ src/allthreads/__errno_location.c
@@ -2,7 +2,7 @@
 
 int *__errno_location(void)
 {
-	static int e;
-	if (libc.main_thread) return __pthread_self()->errno_ptr;
-	return &e;
+	//static int e;
+	//if (libc.main_thread) return __pthread_self()->errno_ptr;
+	//return &e;
 }
--- src/allthreads/cancellation.c
+++ src/allthreads/cancellation.c
@@ -5,15 +5,15 @@ void __do_cleanup_pop();
 
 void _pthread_cleanup_push(struct __ptcb *cb, void (*f)(void *), void *x)
 {
-	cb->__f = f;
-	cb->__x = x;
-	__do_cleanup_push(cb, f, x);
+	//cb->__f = f;
+	//cb->__x = x;
+	//__do_cleanup_push(cb, f, x);
 }
 
 void _pthread_cleanup_pop(struct __ptcb *cb, int run)
 {
-	__do_cleanup_pop(cb, run);
-	if (run) cb->__f(cb->__x);
+	//__do_cleanup_pop(cb, run);
+	//if (run) cb->__f(cb->__x);
 }
 
 static void dummy()
