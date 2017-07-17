6b02c22fd94d807b7d719b8577b35cd3ef83ef20
cmgraff <cm0graff@gmail.com>
2017-07-16 22:54:35 -0500

    improve testing system

--- Makefile
+++ Makefile
@@ -13,12 +13,13 @@ syslibdir = /lib
 
 SRCS = $(sort $(wildcard musllibc/*/*.c graflibc/*/*.c fdlibm/*/*.c))
 OBJS = $(SRCS:.c=.o)
-
+LOBJS = $(OBJS:.o=.lo)
 GENH = include/bits/alltypes.h
 IMPH = musllibc/internal/pthread_impl.h musllibc/internal/libc.h
 
 # test suite
 GCC_WRAP = CC="$(PWD)/usr/bin/gcc-wrap -D_GNU_SOURCE -static" 
+GCC_WRAP_D = CC="$(PWD)/usr/bin/gcc-wrap -D_GNU_SOURCE" 
 TEST_SRCS = $(sort $(wildcard tests/*.c))
 TEST_OBJ = $(TEST_SRCS:.c=) 
 CONTROL_SRCS = $(sort $(wildcard control/*.c))
@@ -40,26 +41,28 @@ RANLIB  = $(CROSS_COMPILE)ranlib
 
 ALL_INCLUDES = $(sort $(wildcard include/*.h include/*/*.h) $(GENH))
 
-EMPTY_LIB_NAMES = m rt util xnet resolv
+EMPTY_LIB_NAMES = m rt pthread crypt util xnet resolv dl
 EMPTY_LIBS = $(EMPTY_LIB_NAMES:%=lib/lib%.a)
 CRT_LIBS = lib/crt1.o lib/Scrt1.o lib/crti.o lib/crtn.o
 STATIC_LIBS = lib/libc.a
-
+SHARED_LIBS = lib/libc.so
 TOOL_LIBS = lib/gcc-wrap.specs
-ALL_LIBS = $(CRT_LIBS) $(STATIC_LIBS)  $(EMPTY_LIBS) $(TOOL_LIBS)
+ALL_LIBS = $(CRT_LIBS) $(STATIC_LIBS) $(SHARED_LIBS) $(EMPTY_LIBS) $(TOOL_LIBS)
 ALL_TOOLS = tools/gcc-wrap
+
+LDSO_PATHNAME = $(syslibdir)/ld-musl-$(ARCH).so.1
 
 -include config.mak
 
 all: $(ALL_LIBS) $(ALL_TOOLS)
 
-install: $(ALL_LIBS:lib/%=$(DESTDIR)$(libdir)/%) $(ALL_INCLUDES:include/%=$(DESTDIR)$(includedir)/%) $(ALL_TOOLS:tools/%=$(DESTDIR)$(bindir)/%) 
+install: $(ALL_LIBS:lib/%=$(DESTDIR)$(libdir)/%) $(ALL_INCLUDES:include/%=$(DESTDIR)$(includedir)/%) $(ALL_TOOLS:tools/%=$(DESTDIR)$(bindir)/%) $(if $(SHARED_LIBS),$(DESTDIR)$(LDSO_PATHNAME),)
 
 clean:
 	rm -f crt/*.o
 	rm -f $(OBJS)
-	
-	rm -f $(ALL_LIBS) lib/.*a
+	rm -f $(LOBJS)
+	rm -f $(ALL_LIBS) lib/*.[ao] lib/*.so
 	rm -f $(ALL_TOOLS)
 	rm -f $(GENH) 
 	rm -f include/bits
@@ -84,6 +87,17 @@ include/bits/alltypes.h: include/bits/alltypes.h.sh
 %.o: %.c $(GENH) $(IMPH)
 	$(CC) $(CFLAGS_ALL_STATIC) -c -o $@ $<
 
+%.lo: $(ARCH)/%.s
+	$(CC) $(CFLAGS_ALL_SHARED) -c -o $@ $<
+
+%.lo: %.c $(GENH) $(IMPH)
+	$(CC) $(CFLAGS_ALL_SHARED) -c -o $@ $<
+
+lib/libc.so: $(LOBJS)
+	$(CC) $(CFLAGS_ALL_SHARED) $(LDFLAGS) -nostdlib -shared \
+	-Wl,-e,_start -Wl,-Bsymbolic-functions \
+	-Wl,-soname=libc.so -o $@ $(LOBJS) -lgcc
+
 lib/libc.a: $(OBJS)
 	rm -f $@
 	$(AR) rc $@ $(OBJS)
@@ -97,20 +111,26 @@ lib/%.o: crt/%.o
 	cp $< $@
 
 lib/gcc-wrap.specs: tools/gcc-wrap.specs.sh config.mak
-	sh $< "$(includedir)" "$(libdir)" > $@
+	sh $< "$(includedir)" "$(libdir)" "$(LDSO_PATHNAME)" > $@
 
 tools/gcc-wrap: config.mak
 	printf '#!/bin/sh\nexec gcc "$$@" -specs "%s/gcc-wrap.specs"\n' "$(libdir)" > $@
 	chmod +x $@
 
 $(DESTDIR)$(bindir)/%: tools/%
-	install -D $< $@ 
+	install -D $< $@
+
+$(DESTDIR)$(libdir)/%.so: lib/%.so
+	install -D -m 755 $< $@
 
 $(DESTDIR)$(libdir)/%: lib/%
 	install -D -m 644 $< $@
 
 $(DESTDIR)$(includedir)/%: include/%
-	install -D -m 644 $< $@ 
+	install -D -m 644 $< $@
+
+$(DESTDIR)$(LDSO_PATHNAME): $(DESTDIR)$(syslibdir)
+	ln -sf $(libdir)/libc.so $@ || true
 
 $(DESTDIR)$(syslibdir):
 	install -d -m 755 $(DESTDIR)$(syslibdir)
@@ -121,6 +141,7 @@ control: $(CONTROL_OBJ)
 
 tests:
 
+	$(MAKE) $(GCC_WRAP_D) testing
 	$(MAKE) $(GCC_WRAP) testing
 	$(MAKE) control 2>/dev/null
 
--- tools/build.sh
+++ tools/build.sh
@@ -1,49 +1,44 @@
 #!/bin/sh
 
-TOOLING="$(pwd)/usr"
-LOGDIR="$(pwd)/logs" 
+TOOLING=$(pwd)/usr
 
+SUF="$(pwd)/logs" 
 make clean 
 ./configure --prefix=${TOOLING} 
-mkdir -p ${LOGDIR}
+mkdir -p ${SUF}
 ./tools/symlink_tests.sh
-printf "\nSo as not to destroy ssh connections automatic builds are logged \n\n"
-printf "Logging to ..  ${LOGDIR}/buildlog \n\n"
-make -j4 > ${LOGDIR}/buildlog 2>&1
+printf "The automatic build is being logged to: ${SUF}/buildlog \n\n"
+make -j4 > ${SUF}/buildlog 2>&1
 make install
-printf "==========COMPILING TEST SUITE==============================\n"
-make tests > ${LOGDIR}/testlog
+printf "==========COMPILING TESTS ===================================\n"
+make tests > ${SUF}/testlog
+printf "=============================================================\n"
+printf "==========TEST RESULT START==================================\n" 
+
+./tests/pow_test 
+
+./tests/cat Makefile > ${SUF}/diff1 2>${SUF}/testerr 
+diff Makefile ${SUF}/diff1 2>&1 > ${SUF}/testerr && \
+echo "The \`cat' utility successfully copied a file" || \
+echo "The cat util failed" 
+
+./control/malloc-driver musllibc > ${SUF}/diff2 2>${SUF}/testerr 
+./tests/malloc-driver musllibc > ${SUF}/diff3 2>${SUF}/testerr 
+diff ${SUF}/diff2 ${SUF}/diff3 2>&1 > ${SUF}/testerr && \
+echo "The \`malloc_driver' test utility successfully iterated through a dir and compared equal to its control method" || \
+echo "The malloc driver failed" 
+
+./control/popen-driver "du musllibc" >${SUF}/diff2 2>${SUF}/testerr 
+./tests/popen-driver "du musllibc" >${SUF}/diff3 2>${SUF}/testerr 
+diff ${SUF}/diff2 ${SUF}/diff3 2>&1 > ${SUF}/testerr && \
+echo "The \`popen_driver' test utility successfully ran \`du' on a directory and compared equal to its control method" || \
+echo "The popen driver failed" 
+
+./control/printf-driver >${SUF}/diff2 2>${SUF}/testerr 
+./tests/printf-driver >${SUF}/diff3 2>${SUF}/testerr 
+diff ${SUF}/diff2 ${SUF}/diff3 2>&1 > ${SUF}/testerr && \
+echo "The \`printf_driver' test utility successfully compared equal to its control method" || \
+echo "The printf driver had a float rounding error "
+
 printf "============================================================\n"
-printf "==========TEST SUITE START==================================\n"
 
-./tests/pow_test
-
-./tests/cat Makefile > ${LOGDIR}/diff1 2>${LOGDIR}/testerr
-
-diff Makefile ${LOGDIR}/diff1 2>&1 > ${LOGDIR}/testerr && echo "The \`cat' utility successfully copied a file" || echo "The cat util failed"
-
-./control/malloc-driver musllibc > ${LOGDIR}/diff2 2>${LOGDIR}/testerr
-
-./tests/malloc-driver musllibc > ${LOGDIR}/diff3 2>${LOGDIR}/testerr
-
-diff ${LOGDIR}/diff2 ${LOGDIR}/diff3 2>&1 > ${LOGDIR}/testerr && echo "The \`malloc_driver' test utility successfully iterated through a dir and compared equal to its control method" || echo "The malloc driver failed"
-
-./control/popen-driver "du musllibc" >${LOGDIR}/diff2 2>${LOGDIR}/testerr
-
-./tests/popen-driver "du musllibc" >${LOGDIR}/diff3 2>${LOGDIR}/testerr
-
-diff ${LOGDIR}/diff2 ${LOGDIR}/diff3 2>&1 > ${LOGDIR}/testerr && echo "The \`popen_driver' test utility successfully ran \`du' on a directory and compared equal to its control method" || echo "The popen driver failed"
-
-
-./control/printf-driver >${LOGDIR}/diff2 2>${LOGDIR}/testerr
-
-./tests/printf-driver >${LOGDIR}/diff3 2>${LOGDIR}/testerr
-
-diff ${LOGDIR}/diff2 ${LOGDIR}/diff3 2>&1 > ${LOGDIR}/testerr && echo "The \`printf_driver' test utility successfully compared equal to its control method" || echo "The printf driver had a float rounding error "
-printf "============================================================\n"
-
-printf "============================================================\n"
-printf "   If all was successful the new compiler wrapper can be\n"
-printf "   invoked as:\n"
-printf "       ${TOOLING}/bin/gcc-wrap\n"
-printf "============================================================\n"
