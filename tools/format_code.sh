584544f2eb5081265179a6fbddd91c6c0969adc9
Zach van Rijn <me@zv.io>
2017-11-14 10:20:25 -0500

    Update formatting script to use new tool 'undo_tidy_rbas.pl'

--- tools/format_code.sh
+++ tools/format_code.sh
@@ -31,6 +31,11 @@ command -v clang-format >/dev/null 2>&1 || { \
 	exit 1
 }
 
+command -v clang-tidy >/dev/null 2>&1 || { \
+	printf "%s" "ERROR: 'clang-tidy' not found." \
+	exit 1
+}
+
 [ -f $ROOT/.clang-format ] || { \
 	printf "%s" "ERROR: '$ROOT/.clang-format' not found." \
 	exit 1
@@ -40,7 +45,22 @@ command -v clang-format >/dev/null 2>&1 || { \
 # ======
 
 for f in $DIRS; do
-	echo "Processing '$f'..."
+	echo "Fixing braces in '$f'..."
+	find "$f" -type f -name "*.h" | xargs clang-tidy \
+		-fix-errors \
+		-checks="readability-braces-around-statements" \
+		>/dev/null 2>&1 > /tmp/rbas_h.log
+	find "$f" -type f -name "*.c" | xargs clang-tidy \
+		-fix-errors \
+		-checks="readability-braces-around-statements" \
+		>/dev/null 2>&1 > /tmp/rbas_c.log
+done
+
+$HERE/undo_tidy_rbas.pl --run /tmp/rbas_h.log && rm /tmp/rbas_h.log
+$HERE/undo_tidy_rbas.pl --run /tmp/rbas_c.log && rm /tmp/rbas_c.log
+
+for f in $DIRS; do
+	echo "Formatting '$f'..."
 	find "$f" -type f -name "*.h" | xargs clang-format -i
 	find "$f" -type f -name "*.c" | xargs clang-format -i
 done
