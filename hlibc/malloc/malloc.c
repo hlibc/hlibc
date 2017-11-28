86df1f348044793a62794e18d887f94997ce9a05
cmgraff <cm0graff@gmail.com>
2017-11-28 16:57:36 -0600

    cleanup malloc.c

--- hlibc/malloc/malloc.c
+++ hlibc/malloc/malloc.c
@@ -6,28 +6,21 @@
 #include <sys/mman.h>
 
 typedef long Align;
-/* for alignment to long boundary */
+
 union header {
-	/* block header */
 	struct {
-		union header *ptr; /* next block if on free list */
-		//unsigned size;
+		union header *ptr;
 		size_t size;
-	/* size of this block */
 	} s;
 	Align x;
-/* force alignment of blocks */
 }header;
 
 typedef union header Header;
 static Header *morecore(unsigned nu);
 void free(void *ap);
 static Header base;
-/* empty list to get started */
 static Header *freep = NULL;
-/* start of free list */
 
-/* malloc: general-purpose storage allocator */
 void *malloc(unsigned nbytes)
 {
 	Header *p, *prevp;
@@ -35,17 +28,14 @@ void *malloc(unsigned nbytes)
 	unsigned nunits;
 	nunits = (nbytes+sizeof(Header)-1)/sizeof(header) + 1;
 	if ((prevp = freep) == NULL) {
-		/* no free list yet */
-		//base.s.ptr = freeptr = prevptr = &base;
 		base.s.ptr = freep = prevp = &base;
-	base.s.size = 0;
+		base.s.size = 0;
 	}
 	for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
-		if (p->s.size >= nunits) { /* big enough */
-			if (p->s.size == nunits) /* exactly */
+		if (p->s.size >= nunits) {
+			if (p->s.size == nunits)
 				prevp->s.ptr = p->s.ptr;
 		else {
-			/* allocate tail end */
 			p->s.size -= nunits;
 			p += p->s.size;
 			p->s.size = nunits;
@@ -53,26 +43,21 @@ void *malloc(unsigned nbytes)
 		freep = prevp;
 		return (void *)(p+1);
 		}
-		if (p == freep) /* wrapped around free list */
+		if (p == freep)
 			if ((p = morecore(nunits)) == NULL)
 				return NULL;
-		/* none left */
 	}
 }
 
-//#define NALLOC 1024
 #define NALLOC 1024
-/* minimum #units to request */
-/* morecore: ask system for more memory */
 static Header *morecore(unsigned nu)
 {
-	char *cp;//, *sbrk(int);
+	char *cp;
 	Header *up;
 	if (nu < NALLOC)
-	nu = NALLOC;
+		nu = NALLOC;
 	cp = sbrk(nu * sizeof(Header));
 	if (cp == (char *) -1)
-		/* no space at all */
 		return NULL;
 		up = (Header *) cp;
 		up->s.size = nu;
@@ -80,25 +65,19 @@ static Header *morecore(unsigned nu)
 	return freep;
 }
 
-/* free: put block ap in free list */
 void free(void *ap)
 {
 	Header *bp, *p;
 	bp = (Header *)ap - 1;
-	/* point to block header */
 	for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
 		if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
-			break; /* freed block at start or end of arena */
-	//if (bp + bp->size == p->s.ptr) {
+			break;
 	if (bp + bp->s.size == p->s.ptr) {
-		/* join to upper nbr */
 		bp->s.size += p->s.ptr->s.size;
 		bp->s.ptr = p->s.ptr->s.ptr;
 	} else
 		bp->s.ptr = p->s.ptr;
-	//if (p + p->size == bp) {
 	if (p + p->s.size == bp) {
-		/* join to lower nbr */
 		p->s.size += bp->s.size;
 		p->s.ptr = bp->s.ptr;
 	} else
@@ -122,3 +101,4 @@ void *calloc(size_t nmemb, size_t size)
 	memset(ret, 0, size);
 	return ret;
 }
+
