687bd68cac942c1b5b14a4df2db6d449f833ef22
cmgraff <cm0graff@gmail.com>
2017-12-01 15:51:18 -0600

    remove weak aliases, and now unfunctioal vm_lock

--- musllibc/mman/madvise.c
+++ musllibc/mman/madvise.c
@@ -2,9 +2,8 @@
 #include "syscall.h"
 #include "libc.h"
 
-int __madvise(void *addr, size_t len, int advice)
+int madvise(void *addr, size_t len, int advice)
 {
 	return syscall(SYS_madvise, addr, len, advice);
 }
 
-weak_alias(__madvise, madvise);
