f51b14042b041419c58509b69c75b632e6cf450a
cmgraff <cm0graff@gmail.com>
2017-12-07 09:26:38 -0600

    whitespace

--- tools/remote_tests.sh
+++ tools/remote_tests.sh
@@ -6,7 +6,6 @@ test_i386()
 }
 test_i386 > test_i386_log 2>&1
 
-
 test_x86_64()
 {
 	ssh cgraff1@gcc75.fsffrance.org -f "$COMMAND"
@@ -18,6 +17,4 @@ test_arm()
 	ssh cgraff1@arm.zv.io -f "$COMMAND"
 }
 test_arm > test_arm_log 2>&1
-
-
 
