0d806a7bd8c9a6668d9694ea82dcb9a7e1feecd6
cmgraff <cm0graff@gmail.com>
2017-12-08 21:44:45 -0600

    wrapper scripts

--- tools/create_wrappers.sh
+++ tools/create_wrappers.sh
@@ -2,12 +2,11 @@
 
 printf "%s\n" "#!/bin/sh" >tools/gcc-wrap
 
-printf "%s\n" "gcc -D_GNU_SOURCE -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/gcc-wrap
+printf "%s\n" "gcc -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/gcc-wrap
 
 printf "%s\n" "#!/bin/sh" >tools/clang-wrap
 
-# Adding _GNU_SOURCE to clang produces tons of errors
-printf "%s\n" "clang -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/clang-wrap
+printf "%s\n" "clang -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/clang-wrap
 
 chmod +x tools/gcc-wrap
 chmod +x tools/clang-wrap
