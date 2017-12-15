91c229909983990bb223a458dfe36a22bdbee6a2
cmgraff <cm0graff@gmail.com>
2017-12-15 02:44:56 -0600

    remove the stray token

--- hlibc/string/strlcpy.c
+++ hlibc/string/strlcpy.c
@@ -1,11 +1,11 @@
 #include <string.h>
 
-size_t strlcpy(char *dst, const char *src, size_t dstsize);
+size_t strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t i = 0;
 
-	for (; i < dstsize-1 && src[i] != '\0'; i++) {
-		dst[i] = src[i]
+	for (; i < dstsize-1  && src[i] != '\0' && dst[i] != '\0'; i++) {
+		dst[i] = src[i];
 	}
 	dst[i++] = '\0';
 
