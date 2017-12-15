e1d7de8a933eae8bc13f0826cfc4de4a999d4a49
cmgraff <cm0graff@gmail.com>
2017-12-15 02:28:29 -0600

    remove the stray token

--- hlibc/string/strlcat.c
+++ hlibc/string/strlcat.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-size_t strlcat(char *dst, const char *src, size_t dstsize);
+size_t strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t len = strnlen(dst, dstsize-1);
 	dstsize -= len;
