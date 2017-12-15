2aa8d7664fb4971901a09811026a9f7c6faa3867
cmgraff <cm0graff@gmail.com>
2017-12-15 10:38:44 -0600

    fix wrappers

--- tools/create_wrappers.sh
+++ tools/create_wrappers.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-printf "%s\n" "#!/bin/sh" >tools/gcc-wrap
+#printf "%s\n" "#!/bin/sh" >tools/gcc-wrap
 
-printf "%s\n" "gcc -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/gcc-wrap
+#printf "%s\n" "gcc -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/gcc-wrap
 
 printf "%s\n" "#!/bin/sh" >tools/clang-wrap
 
 printf "%s\n" "clang -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/clang-wrap
 
-chmod +x tools/gcc-wrap
+#chmod +x tools/gcc-wrap
 chmod +x tools/clang-wrap
