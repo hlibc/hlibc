049b6c4450f536156b60663365f7b8ddf2c02f86
cmgraff <cm0graff@gmail.com>
2017-12-15 02:54:42 -0600

    add a test for strlcpy

--- hlibc/string/strlcpy.c
+++ hlibc/string/strlcpy.c
@@ -4,7 +4,7 @@ size_t strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t i = 0;
 
-	for (; i < dstsize-1  && src[i] != '\0' && dst[i] != '\0'; i++) {
+	for (; i < dstsize-1  && src[i] != '\0' ; i++) {
 		dst[i] = src[i];
 	}
 	dst[i++] = '\0';
---
+++ tests/strlcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char array[1000];
+	char *str = "This is a string to be copied";
+	size_t len = 0;
+	len = strlcpy(array, str, 5);
+	printf("%s\n", array);
+	printf("length %zu\n", len);
+	return 0;
+}
