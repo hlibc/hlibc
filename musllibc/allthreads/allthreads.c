3e4d563f19cc82b8b5e9124c8152fc3ae134cf9a
cmgraff <cm0graff@gmail.com>
2017-12-16 00:36:15 -0600

    remove most of allthreads.c

--- musllibc/allthreads/allthreads.c
+++ musllibc/allthreads/allthreads.c
@@ -3,36 +3,8 @@
 #include "pthread_impl.h"
 #include <stdio.h>
 
-int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void))
-{ 
-	return 0;
-}
-
-void pthread_exit(void *result)
-{
-}
-
-void __do_cleanup_push(struct __ptcb *, void (*)(void *), void *); 
-
-void __do_cleanup_pop(struct __ptcb *, int); 
-
-static int start(void *p)
-{
-} 
-
-int pthread_create(pthread_t *res, const pthread_attr_t *attr, void *(*entry)(void *), void *arg)
-{ 
-	return 0;
-} 
-
-int pthread_join(pthread_t t, void **res)
-{ 
-	return 0;
-} 
-
 static struct pthread main_thread;
 
-/* pthread_key_create.c overrides this */
 static int init_main_thread()
 {
 	if (__set_thread_area(&main_thread) < 0) return -1;
@@ -57,30 +29,4 @@ pthread_t __pthread_self_def()
 	}
 	return __pthread_self();
 }
-
-void pthread_testcancel()
-{
-} 
-
-void __cancel()
-{
-}
-
-int pthread_cancel(pthread_t t)
-{
-	return 0;
-} 
-
-void _pthread_cleanup_push(struct __ptcb *cb, void (*f)(void *), void *x)
-{
-	cb->__f = f;
-	cb->__x = x;
-}
-
-void _pthread_cleanup_pop(struct __ptcb *cb, int run)
-{
-	if (run) cb->__f(cb->__x);
-} 
-
-
 
