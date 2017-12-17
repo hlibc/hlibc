d92eca404a9e233863a2b85e1602e26fcd69e8b4
cmgraff <cm0graff@gmail.com>
2017-12-16 21:53:44 -0600

    fix the clang wrapper. Scrt no longer exists

--- tools/create_wrappers.sh
+++ tools/create_wrappers.sh
@@ -6,7 +6,7 @@
 
 printf "%s\n" "#!/bin/sh" >tools/clang-wrap
 
-printf "%s\n" "clang -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/Scrt1.o $(pwd)/lib/libc.a  " >> tools/clang-wrap
+printf "%s\n" "clang -D_GNU_SOURCE -static -nostdinc -nostdlib -I$(pwd)/include \$@ $(pwd)/lib/crt* $(pwd)/lib/libc.a  " >> tools/clang-wrap
 
 #chmod +x tools/gcc-wrap
 chmod +x tools/clang-wrap
