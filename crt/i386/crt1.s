98ff86cf83614b17e376fd20229ec896b5cbc164
cmgraff <cm0graff@gmail.com>
2017-12-20 03:19:53 +0100

    fix a bad comment

--- crt/i386/crt1.s
+++ crt/i386/crt1.s
@@ -1,20 +1,19 @@
-/* comments taken from crt/x86_64/crt1.s -cmg */
 .weak _init
 .weak _fini
 .text
 .global _start
 _start:
-	xor %ebp,%ebp /* rbp:undefined -> mark as zero 0 (ABI) -cmg*/
+	xor %ebp,%ebp
 	pop %ecx
 	mov %esp,%eax
-	and $-16,%esp /* align stack pointer -cmg*/
+	and $-16,%esp
 	push %esp
 	push %esp
 	push %edx
-	push $_fini /* 5th arg: fini/dtors function -cmg */
-	push $_init /* 4th arg: init/ctors function -cmg *
+	push $_fini
+	push $_init
 	push %eax
 	push %ecx
-	push $main /* 1st arg: application entry ip -cmg */
-	call __libc_start_main /* musl init will run the program -cmg */
+	push $main
+	call __libc_start_main
 1:	jmp 1b
