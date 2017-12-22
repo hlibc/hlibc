0a279ea4f8b9a4ccbf879e98679c69663ecf1908
cmgraff <cm0graff@gmail.com>
2017-12-22 13:21:53 -0600

    use K&R2 original model for _flushbuf

--- hlibc/stdio/_fillbuf.c
+++ hlibc/stdio/_fillbuf.c
@@ -1,23 +1,18 @@
 #include "../internal/internal.h"
 #include <stdlib.h>
-
 int _fillbuf(FILE *fp)
 {
-	int bufsize		   = 0;
-	static char *buffer = NULL;
-	if ((buffer == NULL)) {
-		if (!(buffer = calloc(1,BUFSIZ))) {
-			return EOF;
-		}
-	}
+	int bufsize;
 
 	if ((fp->flags & (_READ | _EOF | _ERR)) != _READ)
 		return EOF;
 	bufsize = (fp->flags & _UNBUF) ? 1 : BUFSIZ;
 	if (fp->buf == NULL)
-		fp->buf = buffer;
-	fp->rp		= fp->buf;
-	fp->len		= read(fp->fd, fp->rp, bufsize);
+		if ((fp->buf =  calloc(1, bufsize)) == NULL)
+			return EOF;
+	
+	fp->rp = fp->buf;
+	fp->len	= read(fp->fd, fp->rp, bufsize);
 
 	if (--fp->len < 0) {
 		if (fp->len == -1)
