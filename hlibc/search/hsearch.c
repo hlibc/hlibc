476783fe77de5b33b57f6ce68caa64c535ed621d
cmgraff <cm0graff@gmail.com>
2017-12-25 11:26:54 -0600

    use tabs

--- hlibc/search/hsearch.c
+++ hlibc/search/hsearch.c
@@ -15,105 +15,105 @@
 
 struct ENTRY_LIST
 {
-    ENTRY entry;
-    struct ENTRY_LIST * next;
+	ENTRY entry;
+	struct ENTRY_LIST * next;
 };
 
 static size_t create_buckets(struct ENTRY_LIST *** buckets, size_t bucket_size)
 {
-    *buckets = calloc(sizeof(struct ENTRY_LIST*), bucket_size);
-    return bucket_size;
+	*buckets = calloc(sizeof(struct ENTRY_LIST*), bucket_size);
+	return bucket_size;
 }
 
 static long int hash_key(const char * key, size_t bucket_size)
 {
-    long int hash;
-    for (hash = 5381; *key != '\0'; ++key) {
-        hash += ((hash << 5) + hash) + *key;
-    }
-    return hash % bucket_size;
+	long int hash;
+	for (hash = 5381; *key != '\0'; ++key) {
+		hash += ((hash << 5) + hash) + *key;
+	}
+	return hash % bucket_size;
 }
 
 int hcreate_r(size_t nel, struct hsearch_data *htab)
 {
-    htab->item_size = 0;
-    htab->bucket_size = create_buckets(&htab->buckets, nel);
-    if (htab->buckets == NULL) {
-        errno = ENOMEM;
-        return 0;
-    }
-    return 1;
+	htab->item_size = 0;
+	htab->bucket_size = create_buckets(&htab->buckets, nel);
+	if (htab->buckets == NULL) {
+		errno = ENOMEM;
+		return 0;
+	}
+	return 1;
 }
 
 int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
-              struct hsearch_data *htab)
+			  struct hsearch_data *htab)
 {
-    long int hash = hash_key(item.key, htab->bucket_size);
-    struct ENTRY_LIST * bucket = htab->buckets[hash];
+	long int hash = hash_key(item.key, htab->bucket_size);
+	struct ENTRY_LIST * bucket = htab->buckets[hash];
 
-    struct ENTRY_LIST * iter = bucket;
-    while (iter != NULL) {
-        if (strcmp(iter->entry.key, item.key) == 0) {
-            break;
-        }
-        iter = iter->next;
-    }
+	struct ENTRY_LIST * iter = bucket;
+	while (iter != NULL) {
+		if (strcmp(iter->entry.key, item.key) == 0) {
+			break;
+		}
+		iter = iter->next;
+	}
 
-    if (iter == NULL && action == FIND) {
-        errno = ESRCH;
-        return 0;
-    }
+	if (iter == NULL && action == FIND) {
+		errno = ESRCH;
+		return 0;
+	}
 
-    if (iter == NULL && action == ENTER) {
-        iter = malloc(sizeof(struct ENTRY_LIST));
-        if (iter == NULL) {
-            errno = ENOMEM;
-            return 0;
-        }
+	if (iter == NULL && action == ENTER) {
+		iter = malloc(sizeof(struct ENTRY_LIST));
+		if (iter == NULL) {
+			errno = ENOMEM;
+			return 0;
+		}
 
-        iter->entry = item;
-        iter->next = bucket;
-        htab->buckets[hash] = iter;
-        htab->item_size++;
-        *retval = &iter->entry;
-        return 1;
-    }
+		iter->entry = item;
+		iter->next = bucket;
+		htab->buckets[hash] = iter;
+		htab->item_size++;
+		*retval = &iter->entry;
+		return 1;
+	}
 
-    *retval = &iter->entry;
-    return 1;
+	*retval = &iter->entry;
+	return 1;
 }
 
 void hdestroy_r(struct hsearch_data *htab)
 {
-    struct ENTRY_LIST * head = NULL;
-    size_t i;
-    for (i = 0; i < htab->bucket_size; ++i) {
-        while (htab->buckets[i] != NULL) {
-            head = htab->buckets[i];
-            htab->buckets[i] = head->next;
-            free(head);
-        }
-    }
-    free(htab->buckets);
-    htab->item_size = 0;
+	struct ENTRY_LIST * head = NULL;
+	size_t i;
+	for (i = 0; i < htab->bucket_size; ++i) {
+		while (htab->buckets[i] != NULL) {
+			head = htab->buckets[i];
+			htab->buckets[i] = head->next;
+			free(head);
+		}
+	}
+	free(htab->buckets);
+	htab->item_size = 0;
 }
 
 struct hsearch_data hash_table;
 
 int hcreate(size_t nel)
 {
-    memset(&hash_table, 0, sizeof(struct hsearch_data));
-    return hcreate_r(nel, &hash_table);
+	memset(&hash_table, 0, sizeof(struct hsearch_data));
+	return hcreate_r(nel, &hash_table);
 }
 
 ENTRY *hsearch(ENTRY item, ACTION action)
 {
-    ENTRY * retval = NULL;
-    int ret = hsearch_r(item, action, &retval, &hash_table);
-    return ret == 0 ? NULL : retval;
+	ENTRY * retval = NULL;
+	int ret = hsearch_r(item, action, &retval, &hash_table);
+	return ret == 0 ? NULL : retval;
 }
 
 void hdestroy(void)
 {
-    hdestroy_r(&hash_table);
+	hdestroy_r(&hash_table);
 }
