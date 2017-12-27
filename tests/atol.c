526ee3f003c87662bcd0b3af9bff467e451cbecd
cmgraff <cm0graff@gmail.com>
2017-12-27 10:49:14 -0600

    be conservative with atoi, atol and atoll tests for now

--- tests/atol.c
+++ tests/atol.c
@@ -8,14 +8,8 @@ int main(int argc, char *argv[])
 	printf("1234567898 = %ld\n", atol("1234567898"));
 	printf("123456789876 = %ld\n", atol("123456789876"));
 	printf("12345678987654321 = %ld\n", atol("12345678987654321"));
-	//LONG_MAX = 9223372036854775807
-	//LONG_MIN = -9223372036854775808
-	printf("LONG_MAX = %ld\n", LONG_MAX);
-	printf("LONG_MIN = %ld\n", LONG_MIN);
-
 	printf("9223372036854775807 %ld\n", atol("9223372036854775807"));
-	printf("-9223372036854775808 %ld\n", atol("-9223372036854775808")); 
-	
+	printf("-9223372036854775808 %ld\n", atol("-9223372036854775808"));
 	printf("123456 %ld\n", atol("123456"));
 	printf("-123456 %ld\n", atol("-123456"));
 	return 0;
--- tests/atoll.c
+++ tests/atoll.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-        //LONG_MAX = 9223372036854775807
-        //LONG_MIN = -9223372036854775808
+
 int main(int argc, char *argv[])
 {
 	printf("123456789 = %lld\n", atoll("123456789"));
@@ -10,13 +9,8 @@ int main(int argc, char *argv[])
 	printf("123456789876 = %lld\n", atoll("123456789876"));
 	printf("12345678987654321 = %lld\n", atoll("12345678987654321"));
 	printf("12345678987654321 = %lld\n", atoll("     12345678987654321"));
-
-	printf("LLONG_MAX = %lld\n", LLONG_MAX);
-	printf("LLONG_MIN = %lld\n", LLONG_MIN);
-
 	printf("9223372036854775807 %lld\n", atoll("9223372036854775807"));
-	printf("-9223372036854775808 %lld\n", atoll("-9223372036854775808")); 
-	
+	printf("-9223372036854775808 %lld\n", atoll("-9223372036854775808"));
 	printf("123456 %lld\n", atoll("123456"));
 	printf("-123456 %lld\n", atoll("-123456"));
 	return 0;
