87cc9120dca0c45ce0421ac9f9f2218df89c2568
Evgeniy Popov <PNDSpirit@gmail.com>
2017-12-30 12:08:19 -0600

    add imaxabs and imaxdiv

---
+++ hlibc/stdlib/imaxdiv.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+
+/*
+	Copyright 2017 Evgeniy Popov
+*/
+
+imaxdiv_t imaxdiv(intmax_t num, intmax_t denom)
+{
+	imaxdiv_t ret;
+	ret.quot = num / denom; 
+	ret.rem = num % denom;
+	return ret;
+}
