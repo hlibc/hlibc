90ac88033042f1b7d5486a77e3536c127df0d9c5
cmgraff <cm0graff@gmail.com>
2017-12-30 10:05:39 -0600

    get rid fo the HARDWARE_MULTIPLIER define

--- hlibc/stdlib/lldiv.c
+++ hlibc/stdlib/lldiv.c
@@ -1,18 +1,11 @@
 #include <stdlib.h>
 /*
-        Copyright 2017 Adrian Parvin D. Ouano
+	Copyright 2017 Adrian Parvin D. Ouano
 */
 lldiv_t lldiv(long long numerator, long long denominator)
 {
 	lldiv_t ret;
-
 	ret.quot = numerator/denominator;
-
-	#ifdef HARDWARE_MULTIPLIER
-	ret.rem = numerator - ret.quot*denominator;
-	#else
 	ret.rem = numerator%denominator;
-	#endif
-
 	return ret;
 }
