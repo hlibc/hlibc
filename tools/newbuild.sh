5147d7106d388943252f349c3f38f9a5ab0e1812
cmgraff <cm0graff@gmail.com>
2018-01-04 21:28:19 -0600

    start bulking out a better testing infrastructure

---
+++ tools/newbuild.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+BASIC_TYPE="	atoi \
+		atol \
+		atoll
+"
+
+TOOLING=$(pwd)/usr
+
+SUF="$(pwd)/logs" 
+make clean 
+CC=$2 ./configure --prefix=${TOOLING} --enable-gcc-wrapper
+mkdir -p ${SUF}
+mkdir -p control
+mkdir -p posix-utils-control
+for i in tests/*.c
+do ln $i control/$(basename $i)
+done
+
+for i in posix-utils/*.c posix-utils/*.h
+do ln $i posix-utils-control/$(basename $i)
+done
+printf "automatic build is being logged to: ${SUF}/buildlog \n\n"
+CC=clang make -j4 > ${SUF}/buildlog #2>&1
+make install
+printf "==========COMPILING TESTS ===================================\n"
+make $1 > ${SUF}/testlog
+printf "=============================================================\n"
+printf "==========TEST RESULT START==================================\n"
+
+for i in $BASIC_TYPE
+do	
+	"./tests/${i}" > "${SUF}/diff2"
+	"./control/${i}" > "${SUF}/diff3"
+	if [ ! -s "${SUF}/diff2" -a ! -s "${SUF}/diff3" ]
+	then	printf "%s\n" "empty test file, something went wrong!!"
+	fi
+	if diff "${SUF}/diff2" "${SUF}/diff3" 2>&1 > "${SUF}/testerr"
+	then	printf "%s\n" "\`${i}' compared equal to its control method"
+	else	printf "%s\n" "##${i} failed to compare equal to its control method"
+	fi
+done
+
+
