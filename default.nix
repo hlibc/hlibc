9726d0bf65830d3d761af89e28e65abf68183283
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-04-25 09:23:21 +0800

    Add default.nix for nix-based building and checking

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ pkgs ? (import <nixpkgs> {})
+, stdenv ? pkgs.stdenv
+, gnumake ? pkgs.gnumake
+}:
+
+stdenv.mkDerivation rec {
+  name = "hlibc";
+  version = "0.0.1";
+
+  src = ./.;
+
+  phases = [ "unpackPhase" "checkPhase" "configurePhase" "installPhase" ];
+  checkPhase = ''
+    make gcctest
+  '';
+
+  doCheck = true;
+
+  hardeningDisable = ["all"];
+
+  buildInputs = [ gnumake ];
+}
