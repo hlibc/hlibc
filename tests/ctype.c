80c28a84305150ad9f1f7d1bccda74458cd2dd6d
cmgraff <cm0graff@gmail.com>
2018-04-26 14:29:16 -0500

    tests for xdigit

--- tests/ctype.c
+++ tests/ctype.c
@@ -14,6 +14,7 @@ int main(void)
 	char cntrlarr[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
 			18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 'p', 'a', 'd', 'd' };
 	char *cntrl = cntrlarr;
+	char *xdigit = "abcdefABCDEF";
 	size_t passes = 0;
 
 
@@ -64,6 +65,12 @@ int main(void)
 		printf("iscntrl pass = %zu\n", passes);
 	}
 	printf("iscntrl should have 32 passes and it has %zu\n", passes);
+
+	for (passes = 0;isxdigit(*xdigit++);++passes)
+	{
+		printf("isxdigit pass = %zu\n", passes);
+	}
+	printf("isxdigit should have 12 passes and it has %zu\n", passes);
 
 
 	return 0;
