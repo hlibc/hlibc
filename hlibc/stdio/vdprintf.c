a795d5912ce56960a0b39b8f89271228d5015a27
cmgraff <cm0graff@gmail.com>
2018-04-26 23:14:50 -0500

    adjust vdprintf to use the new API feature from dprintf

--- hlibc/stdio/vdprintf.c
+++ hlibc/stdio/vdprintf.c
@@ -3,7 +3,7 @@
 int vdprintf(int fd, const char *fmt, va_list argptr)
 {
 	int ret = 0;
-	ret = _printf_inter(stdin + fd, NULL, 0, 0, fmt, argptr);
+	ret = _printf_inter(NULL, NULL, 0, 10 + fd, fmt, argptr);
 	va_end(argptr);
 	return ret;
 }
