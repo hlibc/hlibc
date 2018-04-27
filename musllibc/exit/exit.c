5fada913179229a230afabda39705411ae228db2
cmgraff <cm0graff@gmail.com>
2018-04-27 11:27:38 -0500

    remove some unused variables

--- musllibc/exit/exit.c
+++ musllibc/exit/exit.c
@@ -2,17 +2,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "libc.h"
+
 void __funcs_on_exit();
+
 void exit(int code)
 {
-	static int lock[2];
-
-	/* If more than one thread calls exit, hang until _Exit ends it all */
-
-
-	/* Only do atexit & stdio flush if they were actually used */
 	__funcs_on_exit();
-	//__fflush_on_exit();
+	fflush(NULL);
 
 	/* Destructor s**t is kept separate from atexit to avoid bloat */
 	if (libc.fini) libc.fini();
