eea993515b9a96181affafb1f238f4b4b983adde
cmgraff <cm0graff@gmail.com>
2018-04-27 09:42:05 -0500

    remove useless pointer setting in fclose

--- hlibc/stdio/fclose.c
+++ hlibc/stdio/fclose.c
@@ -5,7 +5,6 @@ int fclose(FILE *fp)
 	int ret = 0;
 	if (fp != NULL) {
 		if ((ret = fflush(fp)) != EOF) {
-			fp->rp = fp->buf = NULL;
 			fp->len = 0;
 			fp->flags &= ~(_READ | _WRITE);
 			if (fp->pid == 0) {
