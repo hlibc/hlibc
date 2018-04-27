5a187e961bfbd43366d540c384206d80ec5afbcb
cmgraff <cm0graff@gmail.com>
2018-04-27 07:23:44 -0500

    use offsets to stdio to represent dprintf's fd number

--- hlibc/stdio/vdprintf.c
+++ hlibc/stdio/vdprintf.c
@@ -3,7 +3,7 @@
 int vdprintf(int fd, const char *fmt, va_list argptr)
 {
 	int ret = 0;
-	ret = _printf_inter(NULL, NULL, 0, 10 + fd, fmt, argptr);
+	ret = _printf_inter(stdout + fd, NULL, 0, 3, fmt, argptr);
 	va_end(argptr);
 	return ret;
 }
