710f53c74e4f40c014d0fabd5d2f9547e70e4ec6
cmgraff <cm0graff@gmail.com>
2018-05-02 05:13:48 -0700

    incomplete fields for renameat. adding a second AT_FDCWD fixes rename() and ./tests/rename-driver.c

--- hlibc/stdio/rename.c
+++ hlibc/stdio/rename.c
@@ -4,10 +4,10 @@
 
 int rename(const char *o, const char *n)
 {
-#ifdef SYS_rename
+#ifdef	SYS_rename
 	return syscall(SYS_rename, o, n);
 #else
-	return syscall(SYS_renameat, AT_FDCWD, o, n);
+	return syscall(SYS_renameat, AT_FDCWD, o, AT_FDCWD, n);
 #endif
 }
 
