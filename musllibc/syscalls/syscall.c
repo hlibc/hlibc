853c8e0663ae8131e8865a8039b66d7969e9b07c
cmgraff <cm0graff@gmail.com>
2018-05-03 12:55:08 -0500

    get rid of the syscall_ret file

--- musllibc/syscalls/syscall.c
+++ musllibc/syscalls/syscall.c
@@ -1,4 +1,15 @@
 #include <stdarg.h>
+#include <errno.h>
+#include <unistd.h>
+
+long __syscall_ret(unsigned long r)
+{
+        if (r > -4096UL) {
+                errno = -r;
+                return -1;
+        }
+        return r;
+}
 
 #undef syscall
 long syscall(long n, ...)
