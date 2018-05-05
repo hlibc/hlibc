da766d0a7eef2bf4f9d99c9146e8bb30b64d301e
cmgraff <cm0graff@gmail.com>
2018-05-05 09:38:30 -0500

    typo in stat

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -8,7 +8,7 @@ int stat(const char *p, struct stat *b)
 #ifdef  SYS_stat
 	return __syscall(SYS_stat, p, b);
 #else
-	return __syscall(SYS_fstatat, AT_FDCWD, p, b 0);
+	return __syscall(SYS_fstatat, AT_FDCWD, p, b, 0);
 #endif
 }
 
