a4ab63f4f4d9958c4f80df227ed7cfa14b159c8c
cmgraff <cm0graff@gmail.com>
2018-05-07 10:08:39 -0500

    strtoll defined twice

--- src/stdlib/atoll.c
+++ src/stdlib/atoll.c
@@ -1,11 +1,5 @@
 #include <ctype.h>
 #include <stdlib.h>
-long long strtoll(const char *s, char **end, int base)
-{
-	long long ans = 0;
-	*end = _tol_driver(s, base, &ans);
-	return ans;
-}
 
 long long atoll(const char *s)
 {
