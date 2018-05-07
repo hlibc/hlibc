5f994279d6ac7f551dbfeff2ab1819d9a9d906e3
cmgraff <cm0graff@gmail.com>
2018-05-07 18:05:47 -0500

    rough out support for hexadecimal and octal

--- src/stdlib/_tol_driver.c
+++ src/stdlib/_tol_driver.c
@@ -18,6 +18,7 @@ char *_tol_driver(const char *s, int base, long long *ans)
 	size_t i = 0;
 	long long ret = 0;
 	long long neg = -1;
+	int lever = 0;
 
 	while (isspace(s[i])) {
 		++i;
@@ -26,15 +27,38 @@ char *_tol_driver(const char *s, int base, long long *ans)
 	switch (s[i]) {
 	case '-':
 		neg = 1;
+		++i;
+		break;
 	case '+':
 		++i;
+		break;
+	case '0':
+		if (base == 16) {
+			switch (s[i]) {
+			case 'x':
+				i += 2;
+				break;
+			default:
+				break;
+			}
+		}
+		if (base == 0){ 
+			++i;
+			base = 8;
+		}
+	default:
+		break;
 	}
 
 	for (; s[i] && isdigit(s[i]); ++i) {
+		lever = 1;
 		ret = (base * ret) - (glph[s[i]]);
 	}
 
-	*ans =  ret * neg;
-	return s + i;
+	*ans = ret * neg;
+	if (lever == 1)
+		return s + i;
+	else
+		return s;
 }
 
