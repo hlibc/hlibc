260f8d81d4a04b3a1fd6bc491c4982310522bdea
cmgraff <cm0graff@gmail.com>
2018-05-07 22:50:31 -0500

    POSIX states that atoll can be implemented in terms of strtoll. use octal for array initializations. check that i>j before returning s+i

--- src/stdlib/atoll.c
+++ src/stdlib/atoll.c
@@ -1,9 +1,9 @@
 #include <ctype.h>
 #include <stdlib.h>
-char *_tol_driver(const char *s, int base, long long *ans);
+
 long long atoll(const char *s)
 {
 	long long ans = 0;
-	_tol_driver(s, 10, &ans);
+	strtol(s, (char **)NULL, 10);
 	return ans;
 }
