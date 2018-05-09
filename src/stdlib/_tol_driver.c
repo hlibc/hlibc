0aabccf1ad8a3cf7b9a9ea3ace53e35d882ed76d
cmgraff <cm0graff@gmail.com>
2018-05-08 20:38:45 -0500

    handle a-z for strtoll's hex. add the copyrights of everyone who worked on strtoll

--- src/stdlib/_tol_driver.c
+++ src/stdlib/_tol_driver.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/*
+	Copyright 2018 CM Graff
+	with various fixes by:
+	Copyright 2018 Tuomo Venäläinen
+	Copyright 2018 Zach van Rijn
+	Copyright 2018 Adrian Parvin D. Ouano
+*/
+
 char *_tol_driver(const char *s, int base, long long *ans)
 {
-	/* glph tab uses octal character representations 
-	of 0-9 and A-Z, 0-9 = '\000'-'\011' which is the
-	49th indice (decimal 48 '0'), A-Z = '\012'-'\055' 
-	which is the 66th indice (decimal 65 'A')  */
+	/* 
+	glph tab uses octal character representations of 0-9, A-Z and a-z. 
+	0-9 = '\000'-'\011' located at the 49th indice (decimal 48 '0')
+	A-Z = '\012'-'\055' located at the 66th indice (decimal 65 'A'),
+	a-z = '\012'-'\055' located at the 98th indice (decimal 97 'a') 
+	*/
 	static uint8_t glph[] = { 
+	/* v    useless filler until the 1st isxdigit */
 	'\000', '\000', '\000', '\000', '\000', '\000',
 	'\000', '\000', '\000', '\000', '\000', '\000',
 	'\000', '\000', '\000', '\000', '\000', '\000',
@@ -28,6 +39,15 @@ char *_tol_driver(const char *s, int base, long long *ans)
 	'\035', '\036', '\037', '\040', '\041', '\042', 
 	'\043', '\044', '\045', '\046', '\047', '\050', 
 	'\051', '\052', '\053', '\054', '\055', '\000',
+	/* a-z start at 98 code points from zero.  v  */
+	'\000', '\000', '\000', '\000', '\000', '\012', 
+	'\013', '\014', '\015', '\016', '\017', '\020', 
+	'\021', '\022', '\023', '\024', '\025', '\026', 
+	'\027', '\030', '\031', '\032', '\033', '\034', 
+	'\035', '\036', '\037', '\040', '\041', '\042', 
+	'\043', '\044', '\045', '\046', '\047', '\050', 
+	'\051', '\052', '\053', '\054', '\055', '\000',
+	/* useless filler starts again here        ^  */
 	'\000', '\000', '\000', '\000', '\000', '\000',
 	'\000', '\000', '\000', '\000', '\000', '\000' };
 
