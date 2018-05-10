246c485d17d9c4f3381fd37c7861529a68e7b1ae
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2018-05-10 12:06:31 +0800

    Deleted excess files

--- hlibc/stdlib/strtoi.c
+++
@@ -1,7 +0,0 @@
-#include <ctype.h>
-#include <stdlib.h>
-
-int strtoi(const char *nptr, char **endptr, int base)
-{
-	return strtoll(nptr, endptr, base);
-}
--- hlibc/stdlib/strtol.c
+++
@@ -1,7 +0,0 @@
-#include <ctype.h>
-#include <stdlib.h>
-
-long strtol(const char *nptr, char **endptr, int base)
-{
-	return strtoll(nptr, endptr, base);
-}
