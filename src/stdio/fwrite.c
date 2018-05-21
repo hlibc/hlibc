9ee8aef255b39faa1198ee7aed4dc55e5d4f9a49
cmgraff <cm0graff@gmail.com>
2018-05-21 03:16:28 -0500

    a more comprehensive test for fwrite which tests line buffering mode

--- src/stdio/fwrite.c
+++ src/stdio/fwrite.c
@@ -3,6 +3,23 @@
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *fp)
 {
 	size_t request = size * nmemb;
-	size_t ret = write(fp->fd, ptr, request);
+	size_t ret = 0;
+	size_t i = 0;
+	size_t j = 0;
+	size_t k = 0;
+	char *t = ptr;
+	if ((fp->flags & _LNBUF)) {
+		for (i=0; i<request;++i, ++k) {
+			if (t[k] == '\n') {
+				ret += write(fp->fd, t + j, i);
+				j = i;
+				i = 0;
+			}
+		}
+	}
+	else {
+		ret = write(fp->fd, ptr, request);
+	}
 	return ret / size;
 }
+
--- tests/fwrite-driver.c
+++ tests/fwrite-driver.c
@@ -3,14 +3,23 @@
 int main(int argc, char *argv[])
 {
 	char buffer[BUFSIZ] = { 0 };
-	ssize_t ret = 0; 
-	if ( argc > 1 ) 
-	{
-		FILE *filepointer = fopen(argv[1], "r"); 
-		while ( (ret = fread(buffer, 1, 1000, filepointer)) > 0 )
-			fwrite(buffer, 1, ret, stdout);
-		fclose(filepointer);
+	size_t frret = 0;
+	size_t fwret = 0;
+	size_t ret = 0;
+	int c = 0;
+	FILE *fp = stdin;
+	if ( argc > 1 ) {
+		fp = fopen(argv[1], "r");
 	}
+	while ((ret = fread(buffer, 1, 10, fp)) > 0 )
+	{
+		frret += ret;
+		fwret += fwrite(buffer, 1, 10, stdout);
+	}
+	if (fp != stdin)
+		fclose(fp);
+	printf("ret val of fread: %zu\n", frret);
+	printf("ret val of fwrite: %zu\n", fwret);
 	return 0;
 }
 
