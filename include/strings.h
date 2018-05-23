7e4b940068d6bd0d9f242fb3543d05614a88c7ce
Bao Hexing <HexingB@qq.com>
2018-05-23 18:28:57 +0800

    Add include/strings.h

---
+++ include/strings.h
@@ -0,0 +1,17 @@
+#ifndef _STRINGS_H
+#define _STRINGS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *index(const char *s, int c);
+
+char *rindex(const char *s, int c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
