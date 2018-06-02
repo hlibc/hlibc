e49379c8ec51b632b6be8a4b373900fa200ead9d
cmgraff <cm0graff@gmail.com>
2018-06-02 03:06:51 -0500

    decrement before populating ungetc

--- src/stdio/ungetc.c
+++ src/stdio/ungetc.c
@@ -4,7 +4,7 @@ int ungetc(int c, FILE *fp)
 {
 	if (c == EOF)
 		return c;
-	*fp->rp-- = (char)c;
+	*--fp->rp = (char)c;
 	fp->flags &= ~_EOF;
 	return c;
 }
--- tests/ungetc.c
+++ tests/ungetc.c
@@ -8,7 +8,7 @@ int main(void)
    
     ungetc('+', stdin);
 
-    //ch = getchar();
+    ch = getchar();
     putchar(ch);
     
     puts("");
