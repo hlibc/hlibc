62ccf961d2d1ff4bb5548d3b969241c9d937488b
cmgraff <cm0graff@gmail.com>
2018-06-02 23:12:37 -0500

    allow for multiple chars to be ungetc'ed

--- src/stdio/getc.c
+++ src/stdio/getc.c
@@ -2,10 +2,8 @@
 
 int getc(FILE *fp)
 {
-	if (fp->ungot > 0)
-	{
-		fp->ungot = 0;
-		return fp->ungotten[0];
+	if (fp->ungot > 0) {
+		return fp->ungotten[fp->ungot-- -1];
 	}
 	if (((fp)->len-- > 1)) {
 		return (unsigned char)*(fp)->rp++;
--- src/stdio/ungetc.c
+++ src/stdio/ungetc.c
@@ -4,10 +4,8 @@ int ungetc(int c, FILE *fp)
 {
 	if (c == EOF)
 		return c;
-	//fp->flags &= ~_EOF;
-	fp->ungot = 1;
-	fp->ungotten[0] = c;
-	
+	fp->flags &= ~_EOF;
+	fp->ungotten[fp->ungot++] = c;
 	return c;
 }
 
