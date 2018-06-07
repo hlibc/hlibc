8be752c610ae02c61d959c3adf4de3afea37ba68
cmgraff <cm0graff@gmail.com>
2018-06-07 14:20:54 -0500

    create a better clang-wrapper

--- tools/create_wrappers.sh
+++ tools/create_wrappers.sh
@@ -2,6 +2,6 @@
 
 printf "%s\n" "#!/bin/sh" >tools/clang-wrap
 
-printf "%s\n" "clang -D_GNU_SOURCE -static -nostdinc -isystem ${1}/include --sysroot ${1}/ \$@ " >> tools/clang-wrap
+printf "%s\n" "clang -D_GNU_SOURCE $SPECIAL -fno-stack-protector -static -nostdinc -isystem ${1}/include --sysroot ${1}/ \$@ " >> tools/clang-wrap
 
 chmod +x tools/clang-wrap
