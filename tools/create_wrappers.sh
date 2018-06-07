bfec7d4908b81d046fc7dcad8f7c00ffca550900
cmgraff <cm0graff@gmail.com>
2018-06-07 15:20:58 -0500

    clang wrapper

--- tools/create_wrappers.sh
+++ tools/create_wrappers.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+
 printf "%s\n" "#!/bin/sh" >tools/clang-wrap
 
-printf "%s\n" "clang -D_GNU_SOURCE $SPECIAL -fno-stack-protector -static -nostdinc -isystem ${1}/include --sysroot ${1}/ \$@ " >> tools/clang-wrap
+printf "%s\n" "clang -D_GNU_SOURCE $3 $4 -fno-stack-protector -static -nostdinc -isystem ${1}/include --sysroot ${1}/ \$@ " >> tools/clang-wrap
 
 chmod +x tools/clang-wrap
