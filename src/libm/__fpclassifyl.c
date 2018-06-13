d0910d0f8dcdf48d8173bc0397b586b8e75ca5cd
lab <cm0graff@gmail.com>
2018-06-13 00:12:05 +0000

    pull in changes to frexpl which should allow aarch64 to work properly

--- src/libm/__fpclassifyl.c
+++ src/libm/__fpclassifyl.c
@@ -1,27 +1,34 @@
 #include "libm.h"
 
 #if LDBL_MANT_DIG == 53 && LDBL_MAX_EXP == 1024
-
+int __fpclassifyl(long double x)
+{
+	return __fpclassify(x);
+}
 #elif LDBL_MANT_DIG == 64 && LDBL_MAX_EXP == 16384
 int __fpclassifyl(long double x)
 {
-	union ldshape u = { x };
-	int e = u.bits.exp;
-	if (!e)
-		return u.bits.m ? FP_SUBNORMAL : FP_ZERO;
+	union ldshape u = {x};
+	int e = u.i.se & 0x7fff;
+	int msb = u.i.m>>63;
+	if (!e && !msb)
+		return u.i.m ? FP_SUBNORMAL : FP_ZERO;
+	if (!msb)
+		return FP_NAN;
 	if (e == 0x7fff)
-		return u.bits.m & (uint64_t)-1>>1 ? FP_NAN : FP_INFINITE;
-	return u.bits.m & (uint64_t)1<<63 ? FP_NORMAL : FP_NAN;
+		return u.i.m << 1 ? FP_NAN : FP_INFINITE;
+	return FP_NORMAL;
 }
 #elif LDBL_MANT_DIG == 113 && LDBL_MAX_EXP == 16384
 int __fpclassifyl(long double x)
 {
-	union ldshape u = { x };
-	int e = u.bits.exp;
+	union ldshape u = {x};
+	int e = u.i.se & 0x7fff;
+	u.i.se = 0;
 	if (!e)
-		return u.bits.mlo | u.bits.mhi ? FP_SUBNORMAL : FP_ZERO;
+		return u.i2.lo | u.i2.hi ? FP_SUBNORMAL : FP_ZERO;
 	if (e == 0x7fff)
-		return u.bits.mlo | u.bits.mhi ? FP_NAN : FP_INFINITE;
+		return u.i2.lo | u.i2.hi ? FP_NAN : FP_INFINITE;
 	return FP_NORMAL;
 }
 #endif
