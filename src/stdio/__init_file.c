05b1beb3bf2d5706b8b33224514af44a72f3f3a7
cmgraff <cm0graff@gmail.com>
2018-06-16 12:46:18 -0500

    respect the off-by-one size_t logic

--- src/stdio/__init_file.c
+++ src/stdio/__init_file.c
@@ -2,9 +2,10 @@
 
 FILE *__init_file(FILE *o)
 {
-        o->read = o->write = o->lnbuf = o->unbuf = o->eof = o->err = o->len = 0;
+        o->read = o->write = o->lnbuf = o->unbuf = o->eof = o->err = 0;
         o->pid = o->ungot = o->ungotten[0] = 0;
         o->buf = o->rp = NULL;
+	o->len = 1; // this is the actual size_t adjusted "zero"
         return o;
 }
 
