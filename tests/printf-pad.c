e8fa2bc2f0af2dd5c2071b347a7e34370be78755
cmgraff <cm0graff@gmail.com>
2018-06-21 06:11:13 -0500

    a set of more comprehensive printf padding tests

--- tests/printf-pad.c
+++ tests/printf-pad.c
@@ -3,12 +3,35 @@
 int main(void)
 {
 	char *s = "printf padded string";
+
+	printf("=====================\n");
+
+	printf("field width %%.5s\n");
 	printf("%.5s\n", s);
+	printf("=====================\n");
+
+	printf("field width %%.*s\n");
 	printf("%.*s\n", 5, s);
+	printf("=====================\n");
+
+	printf("whitespace padding %%Nd\n");
 	printf("%10s\n", "hell");
 	printf("%10d\n", 1234);
+	printf("=====================\n");
 
+	printf("truncate floats\n");
 	printf("%.3f\n", 3.33333);
 	printf("%.10f\n", 3.33333);
+	printf("=====================\n");
+
+	printf("Alternate verse regular form %%#x\n");
+	printf("%x\n", 0xDEADBEEF);
+	printf("%#x\n", 0xDEADBEEF);
+	printf("=====================\n");
+
+	printf("0 padding %%012d\n");
+	printf("%012d\n", 1234);
+	printf("=====================\n");
+
 	return 0;
 }
