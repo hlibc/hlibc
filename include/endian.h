ab9df829240b07a097cb75e2e94cd762deb5a920
James Buren <ryuo@ryuo.xyz>
2018-06-29 00:08:52 -0500

    endian.h: move some macro definitions under the compatibility guard

--- include/endian.h
+++ include/endian.h
@@ -6,10 +6,6 @@
 #define __LITTLE_ENDIAN 1234
 #define __BIG_ENDIAN 4321
 #define __PDP_ENDIAN 3412
-#define BIG_ENDIAN __BIG_ENDIAN
-#define LITTLE_ENDIAN __LITTLE_ENDIAN
-#define PDP_ENDIAN __PDP_ENDIAN
-#define BYTE_ORDER __BYTE_ORDER
 
 #if __STDC_VERSION__ >= 199901L
 inline
@@ -122,6 +118,11 @@ static uint64_t __be_bswap64(uint64_t x)
 }
 
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+#define BIG_ENDIAN __BIG_ENDIAN
+#define LITTLE_ENDIAN __LITTLE_ENDIAN
+#define PDP_ENDIAN __PDP_ENDIAN
+#define BYTE_ORDER __BYTE_ORDER
+
 #define htobe16 __le_bswap16
 #define be16toh __le_bswap16
 #define betoh16 __le_bswap16
