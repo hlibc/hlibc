c787a21466f729068b98a831ae792db5dc416677
James Buren <ryuo@ryuo.xyz>
2018-06-29 01:53:17 -0500

    tests: revise bswap test to have more reusable code

--- tests/bswap.c
+++ tests/bswap.c
@@ -1,23 +1,19 @@
 #include "bswap.h"
 
-int main(void)
-{
 #ifdef _HAS_HLIBC_ENDIAN
-    if(__bswap16(LE16) != BE16)
-    {
-        return 1;
-    }
-
-    if(__bswap32(LE32) != BE32)
-    {
-        return 1;
-    }
-
-    if(__bswap64(LE64) != BE64)
-    {
-        return 1;
-    }
+static const test_case_t test_cases[] =
+{
+    { { __bswap16 }, { LE16 }, { BE16 }, U16 },
+    { { __bswap32 }, { LE32 }, { BE32 }, U32 },
+    { { __bswap64 }, { LE64 }, { BE64 }, U64 }
+};
+static const size_t test_cases_size = sizeof(test_cases) / sizeof(test_cases[0]);
+#else
+static const test_case_t *test_cases = NULL;
+static const size_t test_cases_size = 0;
 #endif
 
-    return 0;
+int main(void)
+{
+    return test_all_cases(test_cases, test_cases_size);
 }
--- tests/bswap.h
+++ tests/bswap.h
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <stddef.h>
 
 enum
 {
@@ -7,5 +8,65 @@ enum
     LE32 = 0x11223344ULL,
     BE32 = 0x44332211ULL,
     LE64 = 0x1122334455667788ULL,
-    BE64 = 0x8877665544332211ULL,
+    BE64 = 0x8877665544332211ULL
 };
+
+typedef struct
+{
+    union
+    {
+        uint16_t (*u16) (uint16_t);
+        uint32_t (*u32) (uint32_t);
+        uint64_t (*u64) (uint64_t);
+    } func;
+    union
+    {
+        uint16_t u16;
+        uint32_t u32;
+        uint64_t u64;
+    } in;
+    union
+    {
+        uint16_t u16;
+        uint32_t u32;
+        uint64_t u64;
+    } out;
+    enum
+    {
+        U16,
+        U32,
+        U64
+    } type;
+} test_case_t;
+
+static int test_one_case(const test_case_t *p, size_t n)
+{
+    const test_case_t *tc = &p[n];
+
+    switch(tc->type)
+    {
+        case U16:
+            return (tc->func.u16(tc->in.u16) != tc->out.u16);
+        case U32:
+            return (tc->func.u32(tc->in.u32) != tc->out.u32);
+        case U64:
+            return (tc->func.u64(tc->in.u16) != tc->out.u64);
+        default:
+            return -1;
+    }
+}
+
+static int test_all_cases(const test_case_t *p, size_t n)
+{
+    size_t i;
+
+    for( i = 0 ; i < n ; ++i )
+    {
+        if(test_one_case(p, i))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
