0425f5551c223eb0adca230fc53b1339a09cd167
James Buren <ryuo@ryuo.xyz>
2018-06-29 02:18:43 -0500

    tests: remove header for bswap test; it's incompatible with existing testsuite

--- tests/bswap.c
+++ tests/bswap.c
@@ -1,19 +1,23 @@
-#include "bswap.h"
+#include <arpa/inet.h>
 
-#ifdef _HAS_HLIBC_ENDIAN
-static const test_case_t test_cases[] =
+enum
 {
-    { { __bswap16 }, { LE16 }, { BE16 }, U16 },
-    { { __bswap32 }, { LE32 }, { BE32 }, U32 },
-    { { __bswap64 }, { LE64 }, { BE64 }, U64 }
+    LE16 = 0x1122ULL,
+    BE16 = 0x2211ULL,
+    LE32 = 0x11223344ULL,
+    BE32 = 0x44332211ULL,
+    LE64 = 0x1122334455667788ULL,
+    BE64 = 0x8877665544332211ULL
 };
-static const size_t test_cases_size = sizeof(test_cases) / sizeof(test_cases[0]);
-#else
-static const test_case_t *test_cases = NULL;
-static const size_t test_cases_size = 0;
-#endif
 
 int main(void)
 {
-    return test_all_cases(test_cases, test_cases_size);
+#ifdef _HAS_HLIBC_ENDIAN
+    return
+        __bswap16(LE16) != BE16 ||
+        __bswap32(LE32) != BE32 ||
+        __bswap64(LE64) != BE64;
+#else
+    return 0;
+#endif
 }
