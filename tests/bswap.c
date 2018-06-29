8e55efabeb217527a29fc730331caf5b4f5536fb
James Buren <ryuo@ryuo.xyz>
2018-06-28 23:48:57 -0500

    tests: add bswap test This tests whether the basic byte swapping functions are working correctly.

---
+++ tests/bswap.c
@@ -0,0 +1,21 @@
+#include <endian.h>
+
+int main(void)
+{
+    if(__bswap16(0x1122) != 0x2211)
+    {
+        return 1;
+    }
+
+    if(__bswap32(0x11223344) != 0x44332211)
+    {
+        return 1;
+    }
+
+    if(__bswap64(0x1122334455667788ULL) != 0x8877665544332211ULL)
+    {
+        return 1;
+    }
+
+    return 0;
+}
