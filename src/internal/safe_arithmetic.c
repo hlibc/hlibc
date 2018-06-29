3dc3cf9d86e1b30b58a85c1c448e554ca4bb4733
James Buren <ryuo@ryuo.xyz>
2018-06-29 14:07:53 -0500

    src/internal/safe_arithmetic.c: remove old unsigned arithmetic code

--- src/internal/safe_arithmetic.c
+++ src/internal/safe_arithmetic.c
@@ -42,60 +42,6 @@ int __safe_umul(uintmax_t a, uintmax_t b, uintmax_t *c, uintmax_t lim)
 	return -1;
 }
 
-#if 0
-int __safe_sub(size_t a, size_t b, size_t *c)
-{
-	if (a >= b)
-	{
-		*c = a - b;
-		return 0;
-	}
-	return -1;
-}
-
-int __safe_add(size_t a, size_t b, size_t *c, size_t lim)
-{
-	size_t tmp = 0;
-	int r = __safe_sub(lim, a, &tmp);
-	if (r == 0 && tmp >= b)
-	{
-		*c = a + b;
-		return 0;
-	}
-	return -1;
-}
-
-int __safe_div(size_t a, size_t b, size_t *c)
-{
-	if (b == 0){
-		/* divide by zero */
-		*c = 0;
-		return 0;
-	} else if ( a < b )
-	{
-		*c = 0;
-		return 0;
-	}
-	*c = a / b;
-	return 0;
-}
-
-int __safe_mul(size_t a, size_t b, size_t *c, size_t lim)
-{ 
-	size_t tmp = 0;
-	__safe_div(lim, a, &tmp);
-	if (tmp >= b) {
-		*c = a * b;
-		return 0;
-	}
-	else if (tmp == 0) {
-		*c = 0;
-		return 0;
-	}
-	return -1;
-}
-#endif
-
 /* old API */
 size_t _safe_subtraction(size_t a, size_t b)
 {
