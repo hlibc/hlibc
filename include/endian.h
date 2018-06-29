5a60325087efc7c6c647d81749b0423f05199356
James Buren <ryuo@ryuo.xyz>
2018-06-28 23:21:13 -0500

    endian.h: reorganize contents Headers and macros are reorganized. GNU_SOURCE macro usage is dropped.

--- include/endian.h
+++ include/endian.h
@@ -1,24 +1,15 @@
 #ifndef _ENDIAN_H
 #define _ENDIAN_H
+#include <bits/endian.h>
+#include <stdint.h>
 
 #define __LITTLE_ENDIAN 1234
 #define __BIG_ENDIAN 4321
 #define __PDP_ENDIAN 3412
-
-#if defined(__GNUC__) && defined(__BYTE_ORDER__)
-#define __BYTE_ORDER __BYTE_ORDER__
-#else
-#include <bits/endian.h>
-#endif
-
-#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
-
 #define BIG_ENDIAN __BIG_ENDIAN
 #define LITTLE_ENDIAN __LITTLE_ENDIAN
 #define PDP_ENDIAN __PDP_ENDIAN
 #define BYTE_ORDER __BYTE_ORDER
-
-#include <stdint.h>
 
 #if __STDC_VERSION__ >= 199901L
 inline
@@ -168,8 +159,6 @@ static uint64_t __be_bswap64(uint64_t x)
 #define htole64(x) __bswap64(x)
 #define le64toh(x) __bswap64(x)
 #define letoh64(x) __bswap64(x)
-#endif
-
 #endif
 
 #endif
