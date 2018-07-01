fe16b599243fa888dcaab13a820928a5f49cb99b
cmgraff <cm0graff@gmail.com>
2018-07-01 13:01:11 -0500

    push the boundary of __safe_sub toward INTMAX_MIN

--- tests/safe_arithmetic.c
+++ tests/safe_arithmetic.c
@@ -14,10 +14,37 @@ int main(void)
 	else
 		printf("Success %jd\n", c);
 	
+	if (__safe_add(INTMAX_MAX, b, &c) == -1)
+                printf("Overflow\n");
+        else
+                printf("Success %jd\n", c);
+
 	if (__safe_sub(a, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
+	
+	if (__safe_sub(INTMAX_MIN, b, &c) == -1)
+		printf("Overflow\n");
+	else
+		printf("Success %jd\n", c);
+	
+	if (__safe_sub(INTMAX_MIN + 101, b, &c) == -1)
+		printf("Overflow\n");
+	else
+		printf("Success %jd\n", c);
+	
+	if (__safe_sub(INTMAX_MIN + 100, b, &c) == -1)
+		printf("Overflow\n");
+	else
+		printf("Success %jd\n", c);
+
+	if (__safe_sub(INTMAX_MIN + 99, b, &c) == -1)
+		printf("Overflow\n");
+	else
+		printf("Success %jd\n", c);
+
+
 #endif
 	
 	return 0;
