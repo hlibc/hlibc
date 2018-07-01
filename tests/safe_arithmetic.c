185d1f2af5bf911eff86b5107bb6fa8e1bf26cca
cmgraff <cm0graff@gmail.com>
2018-07-01 13:50:00 -0500

    safe signed arithmetic is passing basic tests

--- tests/safe_arithmetic.c
+++ tests/safe_arithmetic.c
@@ -9,36 +9,51 @@ int main(void)
 	intmax_t c = 0;
 
 #ifdef _HAS_HLIBC_MATH
+	
+	printf("========================================\n");
+	printf("should equal 200\n");
 	if (__safe_add(a, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
 	
+	printf("========================================\n");
+	printf("should overflow\n");
 	if (__safe_add(INTMAX_MAX, b, &c) == -1)
                 printf("Overflow\n");
         else
                 printf("Success %jd\n", c);
 
+	printf("========================================\n");
+	printf("should equal zero\n");
 	if (__safe_sub(a, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
-	
+	 
+	printf("========================================\n");
+	printf("should overflow\n");
 	if (__safe_sub(INTMAX_MIN, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
 	
+	printf("========================================\n");
+	printf("should not overflow\n");
 	if (__safe_sub(INTMAX_MIN + 101, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
 	
+	printf("========================================\n");
+	printf("should not overflow\n");
 	if (__safe_sub(INTMAX_MIN + 100, b, &c) == -1)
 		printf("Overflow\n");
 	else
 		printf("Success %jd\n", c);
 
+	printf("========================================\n");
+	printf("should overflow\n");
 	if (__safe_sub(INTMAX_MIN + 99, b, &c) == -1)
 		printf("Overflow\n");
 	else
