e4c8d31bd62450d6be25bf6e119b5cd3c3129470
cmgraff <cm0graff@gmail.com>
2018-07-02 16:44:07 -0500

    improve comments on safe arithmetic

--- src/internal/safe_arithmetic.c
+++ src/internal/safe_arithmetic.c
@@ -1,10 +1,6 @@
 #include "internal.h"
 #include <limits.h>
 
-/* new API */
-
-int __safe_uadd(uintmax_t a, uintmax_t b, uintmax_t *c, uintmax_t lim);
-int __safe_usub(uintmax_t a, uintmax_t b, uintmax_t *c);
 int __safe_sub(intmax_t a, intmax_t b, intmax_t *c)
 {
 	/*
@@ -21,11 +17,19 @@ int __safe_sub(intmax_t a, intmax_t b, intmax_t *c)
 
 	/*
 		(-a - b)
-		-500 - +250 == -750
-		SIZE_MIN = 1000
-		-1000 - -500 == -500
-		flip -250
-		-500 < -250 so it fits 
+		 a = -500, b = +250, lim = 1000, t = unknown 
+		1) -500 - +250 = -750
+		   a - b = -750
+
+		2) -1000 - -500 = -500
+		   lim - b = t
+		
+		3) flip -250
+		   b = -b
+		
+		4) if -500 < -250 so it fits 
+		   if t < b
+
 		A negative minus a positive needs the positive flipped
 		and if the positive as a negative is less than the result
 		of the minimum minus the left operand then it fits
@@ -203,51 +207,5 @@ int __safe_umul(uintmax_t a, uintmax_t b, uintmax_t *c, uintmax_t lim)
 		return 0;
 	}
 	return -1;
-}
-
-/* old API */
-size_t _safe_subtraction(size_t a, size_t b)
-{
-	if (a >= b)
-		return a - b;
-	return 0;
-}
-
-size_t _safe_addition(size_t a, size_t b, size_t lim)
-{
-	if (lim - a >= b)
-		return a + b;
-	return 0;
-}
-
-size_t _safe_division(size_t i, size_t x)
-{
-	if (x == 0) {
-		return i; // divide by zero, return original number
-	} else if ( i < x ) {
-		return 0; // division will go past zero, return 0
-	}
-	return i / x; // division fits, perform it
-}
-
-size_t _safe_multiply(size_t i, size_t x, size_t lim)
-{
-	size_t tmp = _safe_division(lim, i);
-	if (tmp >= x) { // Division was greater than or equal to x so
-		return i * x; // so perform the multiplication.
-	}
-	else if (tmp == 0) { // The (uinteger) division won't fit so 0 was returned
-		return lim; // therefore return the maximum saturation
-		//return 0; // therefore return 0 as in multiplication by zero.
-	}
-	return lim; // return the maximum saturation
-}
-
-size_t _safe_upow(size_t i, size_t j)
-{
-	size_t t = 1;
-	while (j--)
-		t += _safe_multiply(t, i, ((size_t)-1));
-	return t;
 }
 
