0b17034312bf08dd450579f40873b668cc278b8f
cmgraff <cm0graff@gmail.com>
2018-07-07 18:40:08 -0500

    set EINVAL for negative base arguments for strtol

--- src/stdlib/_tol_driver.c
+++ src/stdlib/_tol_driver.c
@@ -40,7 +40,7 @@ char *_tol_driver(const char *s, int base, long long *ans)
 	long long neg = -1;
 	uint8_t temp = 0;
 
-	if (base > 36){
+	if (base > 36 && base < 0){
 		errno = EINVAL;
 		return NULL;
 	}
@@ -62,24 +62,24 @@ char *_tol_driver(const char *s, int base, long long *ans)
 	}
 	/* starts with a 0 .. might be hexadecimal or octal */
 	switch (s[j]) {
-		case '0':
-			if (base == 16) {
-				switch (s[j + 1]) {
-				case 'x':
-					j += 2;
-					break;
-				case 'X':
-					j += 2;
-					break;
-				default:
-					break;
-				}
-			}else if (base == 0){ 
-				++j;
-				base = 8;
+	case '0':
+		if (base == 16) {
+			switch (s[j + 1]) {
+			case 'x':
+				j += 2;
+				break;
+			case 'X':
+				j += 2;
+				break;
+			default:
+				break;
 			}
-		default:
-			break;
+		}else if (base == 0){ 
+			++j;
+			base = 8;
+		}
+	default:
+		break;
 	}
 
 	for (i=j; s[i] && isalnum(s[i]) ; ++i) {
