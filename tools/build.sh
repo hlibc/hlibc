2343e492a200a13eefa12afa02ec370167264cfe
cmgraff <cm0graff@gmail.com>
2018-07-14 19:10:31 -0500

    misplaced quote

--- tools/build.sh
+++ tools/build.sh
@@ -21,17 +21,18 @@ git clone https://git.hlibc.xyz/lab/hlibc-test.git
 
 echo "Building GNU bc-1.03 .."
 (
-	cd bc-1.03 && { 
-		sed 's/.*getopt.*//g' proto.h > bak # this is broken in bc-1.03
-		mv bak proto.h
+	cd bc-1.03 && {
+	sed 's/.*getopt.*//g' proto.h > bak # this is broken in bc-1.03
+	mv bak proto.h
 
-		CC="${2}/bin/${1}-wrap" ./configure >/dev/null 2>&1
-		CC="${2}/bin/${1}-wrap make" > /dev/null 2>&1
+	CC="${2}/bin/${1}-wrap" ./configure >/dev/null 2>&1
+	CC="${2}/bin/${1}-wrap" make > /dev/null 2>&1
 
-		printf "\n"
-		printf "1234567*1234567\n" | ./bc -l
-		printf "should be 1524155677489 if bc-1.03 is working\n"
-		printf "\n\n"
+	printf "\n"
+	printf "1234567*1234567\n" | ./bc -l
+	printf "should be 1524155677489 if bc-1.03 is working\n"
+	pwd
+	printf "\n\n"
 	}
 )
 
