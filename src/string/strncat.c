31e867d361194755beabcfe6a54e18ae49a3a4cc
Nicholas Chambers <nchambers@spookyinternet.com>
2018-07-31 20:30:35 -0500

    Rewrite strncat to pass testsuite

--- src/string/strncat.c
+++ src/string/strncat.c
@@ -1,10 +1,19 @@
 #include <stddef.h>
 #include <string.h>
 
-char *strncat(char *s, const char *t, size_t lim)
-{
-	size_t len = strnlen(s, lim);
-	lim -= len;
-	strncpy(s + len, t, lim);
-	return s;
+char *strncat(char *s1, const char *s2, size_t len) {
+    char *dst = s1;
+
+    while(*dst != '\0') {
+        ++dst;
+    }
+
+    size_t count = 0;
+
+    for(; count < len && *s2 != '\0'; ++count) {
+        *dst++ = *s2++;
+    }
+
+    *dst = '\0';
+    return s1;
 }
