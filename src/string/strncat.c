53f8ceb601fb1729d4865330b3d2038034ffcd76
cmgraff <cm0graff@gmail.com>
2018-07-31 22:32:29 -0500

    whitespace

--- src/string/strncat.c
+++ src/string/strncat.c
@@ -1,19 +1,19 @@
 #include <stddef.h>
 #include <string.h>
 
-char *strncat(char *s1, const char *s2, size_t len) {
-    char *dst = s1;
+char *strncat(char *s1, const char *s2, size_t len)
+{
+	char *dst = s1;
+	size_t count = 0;
 
-    while(*dst != '\0') {
-        ++dst;
-    }
+	while(*dst != '\0') {
+		++dst;
+	}
 
-    size_t count = 0;
+	for(; count < len && *s2 != '\0'; ++count) {
+		*dst++ = *s2++;
+	}
 
-    for(; count < len && *s2 != '\0'; ++count) {
-        *dst++ = *s2++;
-    }
-
-    *dst = '\0';
-    return s1;
+	*dst = '\0';
+	return s1;
 }
