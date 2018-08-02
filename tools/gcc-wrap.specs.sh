cfa1b2e666c9f828a73f151e3df4afab0a6602c8
cmgraff <cm0graff@gmail.com>
2018-08-02 18:48:27 -0500

    document the C run time and the gcc specs file a little

--- tools/gcc-wrap.specs.sh
+++ tools/gcc-wrap.specs.sh
@@ -1,3 +1,16 @@
+# Gcc specs file to redirect linking options and handle crt stuffs (C run time)
+
+# This file produces a gcc "specs" file which redirects gcc's linking options
+# ergo allowing a different libc to be used without installing it.
+# In the simplest case, something like: `gcc example.c libc.a crt*.o' would
+# sort of work. But because the compiler needs a specific arrangement of
+# crt1.o (start), crti.o (?) and crti.o (end) to mix with its own internal
+# crtbeginS.o and crtendS.o we instead use the proper method for gcc which is
+# again, the "gcc specs file". The POSIX shell HERE document below expands
+# the specified locations of hlibc's crt files and creates the needed gcc
+# specs file.
+# For clang a different method is used and this file has no effect.
+
 incdir=$1
 libdir=$2
 ldso=$3
@@ -29,11 +42,8 @@ $libdir/crt1.o $libdir/crti.o %{crtbeginS.o}
 
 *esp_link:
 
-
 *esp_options:
 
-
 *esp_cpp_options:
-
 
 EOF
