96595481c1ec4bd922f80babc633c26d238a276b
cmgraff <cm0graff@gmail.com>
2018-10-11 12:54:59 -0500

    use posix naming conventions

--- src/stat/open.c
+++ src/stat/open.c
@@ -3,7 +3,7 @@
 #include <sys/syscall.h>
 #include <stdarg.h>
 
-int open(const char *path, int oflag, ...)
+int open(const char *filename, int oflag, ...)
 {
         va_list i;
         mode_t variadic_mode;
@@ -11,7 +11,7 @@ int open(const char *path, int oflag, ...)
         variadic_mode = va_arg(i, mode_t);
         va_end(i);
 #ifdef SYS_open
-        return syscall(SYS_open, path, oflag|O_LARGEFILE, variadic_mode);
+        return syscall(SYS_open, filename, oflag|O_LARGEFILE, variadic_mode);
 #else
         return syscall(SYS_openat, AT_FDCWD, filename, oflag|O_LARGEFILE, variadic_mode);
 #endif
