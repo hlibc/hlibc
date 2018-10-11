8e8da1ffc4f1f4e0fd873b8298c79990ec2c3cb4
cmgraff <cm0graff@gmail.com>
2018-10-11 18:39:17 -0500

    handle some casting issues in time()

--- src/time/time.c
+++ src/time/time.c
@@ -5,7 +5,7 @@ time_t time(time_t *tloc)
 {
         struct timespec s; 
 	syscall(SYS_clock_gettime, CLOCK_REALTIME, &s);
-	*tloc = s.tv_sec;
-        return *tloc;
+	tloc = (time_t *)s.tv_sec;
+        return s.tv_sec;
 }
 
