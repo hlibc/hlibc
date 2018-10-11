7d1899905ee1112b9fd4c99ca1b11f3ba559a3e7
cmgraff <cm0graff@gmail.com>
2018-10-11 18:31:45 -0500

    remove old comments

--- crt/i386/crt1.s
+++ crt/i386/crt1.s
@@ -1,15 +1,15 @@
 .text
 .global _start
 _start:
-	xor %ebp,%ebp /* rbp:undefined -> mark as zero 0 (ABI) */
+	xor %ebp,%ebp
 	pop %ecx
 	mov %esp,%eax
-	and $-16,%esp /* align stack pointer */
+	and $-16,%esp
 	push %esp
 	push %esp
 	push %edx
 	push %eax
 	push %ecx
-	push $main /* 1st arg: application entry ip */
-	call __load_main /* hlibc init will run the program */
+	push $main
+	call __load_main
 1:	jmp 1b
