9aa777c1e87e0abd98a6092d8c6969724e4e7709
cmgraff <cm0graff@gmail.com>
2018-10-11 18:54:22 -0500

    use hlibc style for inline assembly

--- machine/x86_64/bits/syscall.h
+++ machine/x86_64/bits/syscall.h
@@ -4,8 +4,18 @@ static inline long __internal_syscall(long number, long a, long b, long c, long 
 	register long g __asm__("r8") = e;
 	register long h __asm__("r9") = f;
 	register long i __asm__("r10") = d;
-	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(number), "D"(a), "S"(b),
-						  "d"(c), "r"(i), "r"(g), "r"(h) : "rcx", "r11", "memory");
+	__asm__ __volatile__ ("syscall" :
+		"=a"(ret) :
+		"a"(number),
+		"D"(a),
+		"S"(b),
+		"d"(c),
+		"r"(i),
+		"r"(g),
+		"r"(h) :
+		"rcx",
+		"r11",
+		"memory");
 	return ret;
 }
 
