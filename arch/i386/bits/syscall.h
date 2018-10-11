1881765caff8cb6faf4d92c78f627e92a99e5b12
cmgraff <cm0graff@gmail.com>
2018-10-11 18:28:19 -0500

    use hlibc naming conventions

--- arch/i386/bits/syscall.h
+++ arch/i386/bits/syscall.h
@@ -1,9 +1,9 @@
-static inline long __internal_syscall(long __n, long __a1, long __a2, long __a3, long __a4, long __a5, long __a6)
+static inline long __internal_syscall(long number, long a, long b, long c, long d, long e, long f)
 {
-	unsigned long __ret;
+	unsigned long ret;
 	__asm__ __volatile__ ("pushl %7 ; pushl %%ebp ; mov 4(%%esp),%%ebp ; int $128 ; popl %%ebp ; popl %%ecx"
-		: "=a"(__ret) : "a"(__n), "b"(__a1), "c"(__a2), "d"(__a3), "S"(__a4), "D"(__a5), "g"(__a6) : "memory");
-	return __ret;
+		: "=a"(ret) : "a"(number), "b"(a), "c"(b), "d"(c), "S"(d), "D"(e), "g"(f) : "memory");
+	return ret;
 }
 
 #include <operating_system.h>
