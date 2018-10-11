a761718f967f1168608eb3ac2a9a84e166c7ca5a
cmgraff <cm0graff@gmail.com>
2018-10-11 13:12:56 -0500

    the hlibc syscall rewrite is complete. x86_64 and aarch64 have been tested and work

--- arch/x86_64/bits/syscall.h
+++ arch/x86_64/bits/syscall.h
@@ -1,13 +1,12 @@
-
-static inline long __syscall6(long __n, long __a1, long __a2, long __a3, long __a4, long __a5, long __a6)
+static inline long __syscall6(long number, long a, long b, long c, long d, long e, long f)
 {
-	unsigned long __ret;
-	register long __r10 __asm__("r10") = __a4;
-	register long __r8 __asm__("r8") = __a5;
-	register long __r9 __asm__("r9") = __a6;
-	__asm__ __volatile__ ("syscall" : "=a"(__ret) : "a"(__n), "D"(__a1), "S"(__a2),
-						  "d"(__a3), "r"(__r10), "r"(__r8), "r"(__r9) : "rcx", "r11", "memory");
-	return __ret;
+	unsigned long ret;
+	register long g __asm__("r8") = e;
+	register long h __asm__("r9") = f;
+	register long i __asm__("r10") = d;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(number), "D"(a), "S"(b),
+						  "d"(c), "r"(i), "r"(g), "r"(h) : "rcx", "r11", "memory");
+	return ret;
 }
 
 #include <operating_system.h>
