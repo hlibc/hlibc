b2445fc740fd99dc7604cd771ad0d323210bdc88
cmgraff <cm0graff@gmail.com>
2018-10-11 20:26:24 -0500

    missing return values

--- src/unistd/lseek.c
+++ src/unistd/lseek.c
@@ -5,6 +5,7 @@ off_t lseek(int fildes, off_t offset, int whence)
 {
 #ifdef SYS__llseek
         off_t res;
+	off_t ret;
 	if ((ret = syscall(SYS__llseek, fildes, offset>>32, offset, &res, whence) == -1))
 		ret = res;
 	return ret;
