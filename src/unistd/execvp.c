dcdc4adce0eb06a36431ebea5c3e5e6ba270fca7
cmgraff <cm0graff@gmail.com>
2018-10-13 05:37:31 -0500

    whitespace

--- src/unistd/execvp.c
+++ src/unistd/execvp.c
@@ -6,25 +6,25 @@
 
 int execvp(const char *file, char *const argv[])
 {
-        char word[(PATH_MAX + 1) * 2];
-        char *token;
-        char t[PATH_MAX + 1];
-        char *u;
-        char *p = (char *)file;
-        while (*p++) {
-        	if (*p == '/')
+	char word[(PATH_MAX + 1) * 2];
+	char *token;
+	char t[PATH_MAX + 1];
+	char *u;
+	char *p = (char *)file;
+	while (*p++) {
+		if (*p == '/')
 			return execv(file, argv);
-        }
+	}
 
-        u = getenv("PATH");
-        strcpy(t, u);
-        token = strtok(t, ":");
-        while (token != NULL) {
-                sprintf(word, "%s/%s",token, file);
-                if ( access(word, X_OK) == 0)
+	u = getenv("PATH");
+	strcpy(t, u);
+	token = strtok(t, ":");
+	while (token != NULL) {
+		sprintf(word, "%s/%s",token, file);
+		if ( access(word, X_OK) == 0)
 			return execv(word, argv);
-                token = strtok(NULL, ":");
-        }
-        return -1;
+		token = strtok(NULL, ":");
+	}
+	return -1;
 }
 
