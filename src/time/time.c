6de432ddddb8616c20ce8d2ee2a846a8857845ba
cmgraff <cm0graff@gmail.com>
2018-10-21 17:08:14 -0500

    time(NULL) should not attempt to place a value into its argument

--- src/time/time.c
+++ src/time/time.c
@@ -5,7 +5,8 @@ time_t time(time_t *tloc)
 {
         struct timespec s; 
 	syscall(SYS_clock_gettime, CLOCK_REALTIME, &s);
-	tloc = (time_t *)s.tv_sec;
+	if (tloc != NULL)
+		tloc = (time_t *)s.tv_sec;
         return s.tv_sec;
 }
 
