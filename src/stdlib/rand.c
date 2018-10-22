0b82c7a2d2b49473c4aa7fba704b112ab793b305
cmgraff <cm0graff@gmail.com>
2018-10-21 21:10:19 -0500

    rand was using the wrong data types. use unsigned for the cache

--- src/stdlib/rand.c
+++ src/stdlib/rand.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-static ssize_t cache = 1;
+unsigned long cache = 1;
 
 int rand(void)
 {
 	cache = cache * 1103515245 + 12345;
-	return (cache / 65536) % 32768;
+	return (cache/65536) % 32768;
 }
 
 void srand(unsigned int seed)
 {
 	cache = seed;
 }
-/*
-	Warning!! FIXME!! 
-	using rand and srand for rand and srandom is wrong. But hlibc
-	needs this for now in order to build GNU bc
-*/
+
 long int random(void)
 {
 	return rand();
