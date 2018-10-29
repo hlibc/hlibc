4ec0cdd9815cff2d7a76b4855c1086e30bd9a820
graff <cm0graff@gmail.com>
2018-10-29 11:42:23 -0500

    comments explaining why fputs/puts can't be implemented in terms of fprintf

--- src/stdio/__puts_inter.c
+++ src/stdio/__puts_inter.c
@@ -2,6 +2,10 @@
 
 int __puts_inter(const char *s, FILE *fp, int append)
 {
+	/* puts/fputs can't be implemented in terms of a call to fprintf because
+	 * the printf family of function is only specified to handle INT_MAX
+	 * number of bytes whereas puts has no such retriction.
+	 */
 	int c;
 	while ((c = *s++)) {
 		putc(c, fp);
