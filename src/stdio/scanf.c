f8a089740faec914dea3189f17c2d2b5ea092aae
graff <cm0graff@gmail.com>
2018-11-02 06:24:30 -0500

    a loop body for fscanf

--- src/stdio/scanf.c
+++ src/stdio/scanf.c
@@ -1,8 +1,20 @@
 #include "../internal/internal.h"
+#include <limits.h>
 
 
 
 int fscanf(FILE *restrict stream, const char *restrict format, ...)
 {
+	char *p = NULL;
+	size_t i = 0;
+	size_t bound = INT_MAX;
+	
+	for (p = (char *)format; *p && i < bound; p++) {
+		if (*p != '%') {
+			   fputc(*p, stream);
+			   continue;
+		}
+		//++p;
+	}
 	return 0;
 }
