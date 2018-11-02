43d3b8d86c8d85578e5e87a4cb970842c6a1d303
graff <cm0graff@gmail.com>
2018-11-02 18:19:07 -0500

    the strtol bug which was preventing atoll to be implemented appears to be fixed

--- src/stdlib/atoll.c
+++ src/stdlib/atoll.c
@@ -1,9 +1,9 @@
 #include <ctype.h>
 #include <stdlib.h>
-char *__tol_inter(const char *, int, long long *);
+
 long long atoll(const char *s)
 {
 	long long ans = 0;
-	__tol_inter(s, 10, &ans);
+	ans = strtoll(s, NULL, 10);
 	return ans;
 }
