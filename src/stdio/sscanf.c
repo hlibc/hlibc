416050936f4cc34d9f0c9c35c3cc75f372fbc3d8
graff <cm0graff@gmail.com>
2018-11-05 07:24:45 -0600

    sscanf should have a NULL argument to the __fscanf_inter FILE object argument

--- src/stdio/sscanf.c
+++ src/stdio/sscanf.c
@@ -4,7 +4,7 @@
 int vsscanf(const char *str, const char *fmt, va_list ap)
 {
 	int ret = 0;
-	ret = __fscanf_inter(str, stdin, fmt, ap);
+	ret = __fscanf_inter(str, NULL, fmt, ap);
 	va_end(ap);
 	return ret;
 }
