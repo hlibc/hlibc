5e28d01fbf656e0be89eda55e46aedda3d19b169
graff <cm0graff@gmail.com>
2018-12-11 17:41:55 -0600

    provide data type overflow checks for clock()

--- src/time/clock.c
+++ src/time/clock.c
@@ -1,11 +1,18 @@
 #include <time.h>
 #include <sys/syscall.h>
-// TODO: provide arithmetic boundary checks and return a -1
+#include "../internal/internal.h"
+#include <limits.h>
+
 clock_t clock()
 {
         struct timespec t = {0};
+	long total = 0;
         if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t))
-                return -1; 
-        return (t.tv_sec * 1000000) + (t.tv_nsec / 1000);
+                return -1;
+	if (__safe_umul_sz(t.tv_sec, 1000000, (size_t *)&total, LONG_MAX) == -1)
+		return -1;
+	if (__safe_uadd_sz(total, t.tv_nsec / 1000, (size_t *)&total, LONG_MAX) == -1)
+		return -1;
+        return total;
 }
 
