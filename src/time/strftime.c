bc4ddc9d7ab80d3a09d735ed69526c05f017a1f3
graff <cm0graff@gmail.com>
2019-01-30 13:49:41 -0600

    whitespace. rename variables

--- src/time/strftime.c
+++ src/time/strftime.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-size_t strftime(char *dp, size_t maxsize, const char *fp, const struct tm *timeptr)
+size_t strftime(char *s, size_t max, const char *fmt, const struct tm *t)
 {
 	static char *__days[] = {
 		"Sunday",
@@ -33,135 +33,134 @@ size_t strftime(char *dp, size_t maxsize, const char *fp, const struct tm *timep
 	size_t len = 0;
 	char tmpbuf[64];
 
-	if (fp == NULL || dp == NULL || maxsize <= 0)
+	if (fmt == NULL || s == NULL || max <= 0)
 		return 0;
 
-	while (*fp) {
-		if (*fp != '%') {
-			if (ret < maxsize) {
-				*dp++ = (*fp++);
+	while (*fmt) {
+		if (*fmt != '%') {
+			if (ret < max) {
+				*s++ = (*fmt++);
 				ret++;
 			}
 			continue;
 		}
 
-		switch(*++fp) { 
+		switch(*++fmt) { 
 			case 'a':
-				p = __days[timeptr->tm_wday];
+				p = __days[t->tm_wday];
 				len = 3;
 				goto dostrn_deep;
 			case 'A':
-				p = __days[timeptr->tm_wday];
+				p = __days[t->tm_wday];
 				goto dostr_deep;
 			case 'b':
-				p = __mons[timeptr->tm_mon];
+				p = __mons[t->tm_mon];
 				len = 3;
 				goto dostrn_deep;
 			case 'B':
-				p = __mons[timeptr->tm_mon];
+				p = __mons[t->tm_mon];
 				goto dostr_deep;
 			case 'c': 
-				r = strftime(dp, maxsize - ret, "%X %x", timeptr);
+				r = strftime(s, max - ret, "%X %x", t);
 				if (r <= 0) 
 					return r;
-				dp += r;
+				s += r;
 				ret += r;
 				break;
 			case 'd':
-				sprintf(tmpbuf, "%02d", timeptr->tm_mday); 
+				sprintf(tmpbuf, "%02d", t->tm_mday); 
 				goto dostr;
 			case 'e':
-				sprintf(tmpbuf, "%2d", timeptr->tm_mday); 
+				sprintf(tmpbuf, "%2d", t->tm_mday); 
 				goto dostr;
 			case 'H':
-				sprintf(tmpbuf, "%02d", timeptr->tm_hour); 
+				sprintf(tmpbuf, "%02d", t->tm_hour); 
 				goto dostr;
 			case 'I':
-				sprintf(tmpbuf, "%02d", ((timeptr->tm_hour + 11) % 12) + 1); 
+				sprintf(tmpbuf, "%02d", ((t->tm_hour + 11) % 12) + 1); 
 				goto dostr;
 			case 'j':
-				sprintf(tmpbuf, "%03d", timeptr->tm_yday + 1); 
+				sprintf(tmpbuf, "%03d", t->tm_yday + 1); 
 				goto dostr;
 			case 'm':
-				sprintf(tmpbuf, "%02d", timeptr->tm_mon + 1); 
+				sprintf(tmpbuf, "%02d", t->tm_mon + 1); 
 				goto dostr;
 			case 'M':
-				sprintf(tmpbuf, "%02d", timeptr->tm_min); 
+				sprintf(tmpbuf, "%02d", t->tm_min); 
 				goto dostr;
 			case 'p':
-				if (timeptr->tm_hour < 12)
+				if (t->tm_hour < 12)
 					p = "AM";
 				else
 					p = "PM";
 				goto dostr;
 			case 'S':
-				sprintf(tmpbuf, "%02d", timeptr->tm_sec); 
+				sprintf(tmpbuf, "%02d", t->tm_sec); 
 				goto dostr;
 			case 'U':
 			case 'W':
-				vary = timeptr->tm_wday - timeptr->tm_yday % 7;
-				if (*fp == 'W')
+				vary = t->tm_wday - t->tm_yday % 7;
+				if (*fmt == 'W')
 					vary--;
 				vary = (vary + 7) % 7;
-				sprintf(tmpbuf, "%02d", (timeptr->tm_yday + vary) / 7); 
+				sprintf(tmpbuf, "%02d", (t->tm_yday + vary) / 7); 
 				goto dostr;
 			case 'w':
-				sprintf(tmpbuf, "%02d", timeptr->tm_wday); 
+				sprintf(tmpbuf, "%02d", t->tm_wday); 
 				goto dostr;
 			case 'x':
-				r = strftime(dp, maxsize - ret, "%B %d, %Y", timeptr);
+				r = strftime(s, max - ret, "%B %d, %Y", t);
 				if (r <= 0)
 					return r;
-				dp += r;
+				s += r;
 				ret += r;
 				break;
 			case 'X':
-				r = strftime(dp, maxsize - ret, "%H:%M:%S", timeptr);
+				r = strftime(s, max - ret, "%H:%M:%S", t);
 				if (r <= 0)
 					return r;
-				dp += r;
+				s += r;
 				ret += r;
 				break;
 			case 'y':
-				sprintf(tmpbuf, "%02d", timeptr->tm_year % 100); 
+				sprintf(tmpbuf, "%02d", t->tm_year % 100); 
 				goto dostr;
 			case 'Y':
-				sprintf(tmpbuf, "%d", timeptr->tm_year + 1900); 
+				sprintf(tmpbuf, "%d", t->tm_year + 1900); 
 				goto dostr;
 			case 'Z':
 				break;
 			case '%':
-				if (ret < maxsize) {
-					*dp++ = ('%');
+				if (ret < max) {
+					*s++ = ('%');
 					ret++;
 				}
 				break;
 			dostr:
 				p = tmpbuf;
 			dostr_deep:
-				len = maxsize;
+				len = max;
 				goto dostrn_deep;
 			dostrn:
 				p = tmpbuf;
 			dostrn_deep:
 				while (len-- > 0 && *p) {
-					if (ret < maxsize) {
-						*dp++ = (*p++);
+					if (ret < max) {
+						*s++ = (*p++);
 						ret++;
 					}
 				}
 				break;
 		}
-
-		fp++;
+		fmt++;
 	}
 	
-	if (ret >= maxsize) {
-		dp[maxsize - 1] = 0;
+	if (ret >= max) {
+		s[max - 1] = 0;
 		return 0;
 	}
 
-	*dp = 0;
+	*s = 0;
 
 	return ret;
 }
