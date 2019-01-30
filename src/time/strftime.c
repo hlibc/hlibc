1058e6255f63387642c63762635abe350c10443f
graff <cm0graff@gmail.com>
2019-01-30 08:18:22 -0600

    any 'case' with a 'goto' at the end does not also need a 'break'

--- src/time/strftime.c
+++ src/time/strftime.c
@@ -49,77 +49,63 @@ size_t strftime(char *dp, size_t maxsize, const char *fp, const struct tm *timep
 		}
 
 		switch(*++fp)
-		{
-
+		{ 
 			case 'a':
 				p = weekdays[timeptr->tm_wday];
 				len = 3;
 				goto dostrn;
-				break; 
 			case 'A':
 				p = weekdays[timeptr->tm_wday];
 				goto dostr;
-				break; 
 			case 'b':
 				p = months[timeptr->tm_mon];
 				len = 3;
 				goto dostrn;
-				break; 
 			case 'B':
 				p = months[timeptr->tm_mon];
 				goto dostr;
-				break; 
 			case 'c': 
 				r = strftime(dp, maxsize - ret, "%X %x", timeptr);
 				if(r <= 0) 
 					return r;
 				dp += r;
 				ret += r;
-				break; 
+				break;
 			case 'd':
 				sprintf(tmpbuf, "%02d", timeptr->tm_mday);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'e':
 				sprintf(tmpbuf, "%2d", timeptr->tm_mday);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'H':
 				sprintf(tmpbuf, "%02d", timeptr->tm_hour);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'I':
 				sprintf(tmpbuf, "%02d", ((timeptr->tm_hour + 11) % 12) + 1);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'j':
 				sprintf(tmpbuf, "%03d", timeptr->tm_yday + 1);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'm':
 				sprintf(tmpbuf, "%02d", timeptr->tm_mon + 1);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'M':
 				sprintf(tmpbuf, "%02d", timeptr->tm_min);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'p':
 				p = timeptr->tm_hour < 12 ? "AM" : "PM";
-				goto dostr; 
-				break; 
+				goto dostr;
 			case 'S':
 				sprintf(tmpbuf, "%02d", timeptr->tm_sec);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'U':
 			case 'W':
 				vary = timeptr->tm_wday - timeptr->tm_yday % 7;
@@ -129,36 +115,32 @@ size_t strftime(char *dp, size_t maxsize, const char *fp, const struct tm *timep
 				sprintf(tmpbuf, "%02d", (timeptr->tm_yday + vary) / 7);
 				p = tmpbuf;
 				goto dostr;
-				break;
 			case 'w':
 				sprintf(tmpbuf, "%02d", timeptr->tm_wday);
 				p = tmpbuf;
 				goto dostr;
-				break; 
 			case 'x':
 				r = strftime(dp, maxsize - ret, "%B %d, %Y", timeptr);
 				if (r <= 0)
 					return r;
 				dp += r;
 				ret += r;
-				break; 
+				break;
 			case 'X':
 				r = strftime(dp, maxsize - ret, "%H:%M:%S", timeptr);
 				if (r <= 0)
 					return r;
 				dp += r;
 				ret += r;
-				break; 
+				break;
 			case 'y':
 				sprintf(tmpbuf, "%02d", timeptr->tm_year % 100);
 				p = tmpbuf;
 				goto dostr;
-				break;
 			case 'Y':
 				sprintf(tmpbuf, "%d", timeptr->tm_year + 1900);
 				p = tmpbuf;
 				goto dostr;
-				break;
 			case 'Z':
 				break;
 			case '%':
