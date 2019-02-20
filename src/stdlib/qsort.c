2e2a3c40072c635adc674ae0fd82cb68fb1bb7a7
James Buren <ryuo@ryuo.xyz>
2019-02-20 14:19:54 -0600

    qsort: implement siftdown

--- src/stdlib/qsort.c
+++ src/stdlib/qsort.c
@@ -68,8 +68,35 @@ static size_t rightchild(size_t i)
 	return (i * 2) + 2;
 }
 
-static void siftdown(qsort_t *qs, size_t start, size_t end)
+static void siftdown(qsort_t *qs, size_t root, size_t end)
 {
+	size_t node;
+	size_t lchild;
+	size_t rchild;
+
+	node = root;
+
+	while((rchild = rightchild(node)) <= end)
+	{
+		lchild = leftchild(node);
+		node = (compare(qs, rchild, lchild) > 0) ? rchild : lchild;
+	}
+
+	if((lchild = leftchild(node)) <= end)
+	{
+		node = lchild;
+	}
+
+	while(compare(qs, root, node) > 0)
+	{
+		node = parent(node);
+	}
+
+	while(node > root)
+	{
+		swap(qs, node, root);
+		node = parent(node);
+	}
 }
 
 static void heapify(qsort_t *qs)
