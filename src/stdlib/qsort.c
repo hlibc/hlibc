f692311eb25ce78dea3618da613f79b2fba81bdd
James Buren <ryuo@ryuo.xyz>
2019-02-20 10:12:34 -0600

    qsort: use a shorter name for the internal type

--- src/stdlib/qsort.c
+++ src/stdlib/qsort.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 
-struct qsort
+typedef struct
 {
 	char* arr;
 	size_t len;
 	size_t size;
 	int (*cmp) (const void *, const void *);
-};
+} qsort_t;
 
-static char *element(struct qsort *qs, size_t i)
+static char *element(qsort_t *qs, size_t i)
 {
 	return &qs->arr[qs->size * i];
 }
 
-static void swap(struct qsort *qs, size_t A, size_t B)
+static void swap(qsort_t *qs, size_t A, size_t B)
 {
 	char *a = element(qs, A);
 	char *b = element(qs, B);
@@ -30,7 +30,7 @@ static void swap(struct qsort *qs, size_t A, size_t B)
 
 void qsort(void *base, size_t nmemb, size_t size, int (*compar) (const void *, const void *))
 {
-	struct qsort qs;
+	qsort_t qs;
 
 	qs.arr = base;
 	qs.len = nmemb;
